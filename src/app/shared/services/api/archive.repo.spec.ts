a294de4ce5088e3ece2d37d90cc5621732887c30
Andrew Atwood <sandurz@gmail.com>
2018-07-19 11:21:20 -0500

    stub out archive repo and tests

---
+++ src/app/shared/services/api/archive.repo.spec.ts
@@ -0,0 +1,61 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from '../../../../environments/environment';
+
+import { TEST_DATA, TEST_DATA_2 } from '../../../core/core.module.spec';
+import { HttpService, Observable } from '../http/http.service';
+import { ArchiveRepo, ArchiveResponse } from './archive.repo';
+import { SimpleVO, AccountPasswordVO, AccountVO, ArchiveVO } from '../../../models';
+
+describe('ArchiveRepo', () => {
+  let repo: ArchiveRepo;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [HttpService]
+    });
+
+    repo = new ArchiveRepo(TestBed.get(HttpService));
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should get a single archive', () => {
+    const expected = require('../../../../test/responses/archive.get.single.success.json');
+
+    repo.get([TEST_DATA.archive.archiveId])
+    .subscribe((response) => {
+      expect(response.getArchiveVO().archiveId).toEqual(TEST_DATA.archive.archiveId);
+      expect(response.getArchiveVO().archiveNbr).toEqual(TEST_DATA.archive.archiveNbr);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUrl}/archive/get`);
+    req.flush(expected);
+  });
+
+  it('should get multiple archives', () => {
+    const expected = require('../../../../test/responses/archive.get.multiple.success.json');
+
+    repo.get([TEST_DATA.archive.archiveId, TEST_DATA_2.archive.archiveId])
+    .subscribe((response) => {
+      const archives = response.getArchiveVOs();
+      expect(archives.length).toBe(2);
+
+      expect(archives[0].archiveId).toEqual(TEST_DATA.archive.archiveId);
+      expect(archives[0].archiveNbr).toEqual(TEST_DATA.archive.archiveNbr);
+
+      expect(archives[1].archiveId).toEqual(TEST_DATA_2.archive.archiveId);
+      expect(archives[1].archiveNbr).toEqual(TEST_DATA_2.archive.archiveNbr);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUrl}/archive/get`);
+    req.flush(expected);
+  });
+});
--- src/app/shared/services/api/index.repo.ts
+++ src/app/shared/services/api/index.repo.ts
@@ -1,2 +1,3 @@
 export * from './auth.repo';
 export * from './account.repo';
+export * from './archive.repo';
