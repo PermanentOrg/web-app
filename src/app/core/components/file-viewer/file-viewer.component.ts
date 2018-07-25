3a72f26d8efbd3e1bdd6da2d936876859b3057e3
Andrew Atwood <sandurz@gmail.com>
2018-07-24 19:24:37 -0500

    overflow hidden on body during fileviewer

--- src/app/core/components/file-viewer/file-viewer.component.ts
+++ src/app/core/components/file-viewer/file-viewer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef, Inject} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
 import { RecordVO } from '@root/app/models';
 
@@ -7,14 +8,23 @@ import { RecordVO } from '@root/app/models';
   templateUrl: './file-viewer.component.html',
   styleUrls: ['./file-viewer.component.scss']
 })
-export class FileViewerComponent implements OnInit {
+export class FileViewerComponent implements OnInit, OnDestroy {
   record: RecordVO;
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  private viewerElement: Element;
+  private bodyScroll: number;
+
+  constructor(private router: Router, private route: ActivatedRoute, private element: ElementRef, @Inject(DOCUMENT) private document: any) {
     this.record = route.snapshot.data.currentRecord;
   }
 
   ngOnInit() {
+    this.viewerElement = this.element.nativeElement.querySelector('.file-viewer');
+    this.document.body.style.setProperty('overflow', 'hidden');
+  }
+
+  ngOnDestroy() {
+    this.document.body.style.setProperty('overflow', '');
   }
 
   close() {
