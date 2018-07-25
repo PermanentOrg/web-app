e4725243efc78ab0ed65271f4a4fbf067d01af9b
Andrew Atwood <sandurz@gmail.com>
2018-07-25 14:03:56 -0500

    pass unit tests for services

--- src/app/core/components/file-list/file-list.component.spec.ts
+++ src/app/core/components/file-list/file-list.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FileListComponent } from '@core/components/file-list/file-list.component';
 
-xxdescribe('FileListComponent', () => {
+xdescribe('FileListComponent', () => {
   let component: FileListComponent;
   let fixture: ComponentFixture<FileListComponent>;
 
--- src/app/core/resolves/folder-resolve.service.spec.ts
+++ src/app/core/resolves/folder-resolve.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
+import * as Testing from '@root/test/testbedConfig';
 
 import { FolderResolveService } from '@core/resolves/folder-resolve.service';
 
 describe('FolderResolveService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [FolderResolveService]
-    });
+    const config = Testing.BASE_TEST_CONFIG;
+    const providers = config.providers as any[];
+    providers.push(FolderResolveService);
+    TestBed.configureTestingModule(config);
   });
 
   it('should be created', inject([FolderResolveService], (service: FolderResolveService) => {
