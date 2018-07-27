3d68149be18a09bab384d1805dc203ba078babdd
Andrew Atwood <sandurz@gmail.com>
2018-07-27 08:26:19 -0500

    set API keys for different environments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiKey: '`2QOGhZvC.1.VdaoPn;-gIhi',
+  apiKey: 'wlK?anNl,BW%W#5;FJN11qGn',
   apiUrl: 'https://dev.permanent.org/api',
   hmr: false
 };
