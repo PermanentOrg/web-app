b6e84b3a376e3b3e0e9967919df3d418281f9417
Andrew Atwood <sandurz@gmail.com>
2018-07-27 08:24:10 -0500

    broken test path

--- src/app/shared/components/thumbnail/thumbnail.component.spec.ts
+++ src/app/shared/components/thumbnail/thumbnail.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ThumbnailComponent } from '@core/components/thumbnail/thumbnail.component';
+import { ThumbnailComponent } from '@shared/components/thumbnail/thumbnail.component';
 
 xdescribe('ThumbnailComponent', () => {
   let component: ThumbnailComponent;
