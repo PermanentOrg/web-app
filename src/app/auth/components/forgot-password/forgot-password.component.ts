491b2f8455c81a6d605dbb9343dcd94e31aa1e0d
Andrew Atwood <sandurz@gmail.com>
2018-08-06 10:28:29 -0500

    don't prefill email on forgot password form

--- src/app/auth/components/forgot-password/forgot-password.component.ts
+++ src/app/auth/components/forgot-password/forgot-password.component.ts
@@ -8,16 +8,10 @@ import { AuthResponse } from '@shared/services/api/auth.repo';
 import { MessageService } from '@shared/services/message/message.service';
 import { ApiService } from '@shared/services/api/api.service';
 
-const MIN_PASSWORD_LENGTH = 10;
-
 const FORM_ERROR_MESSAGES = {
   email: {
     email: 'Invalid email address',
     required: 'Email required'
-  },
-  password: {
-    minlength: `Passwords must be ${MIN_PASSWORD_LENGTH} characters`,
-    required: 'Password required'
   }
 };
 
@@ -42,7 +36,7 @@ export class ForgotPasswordComponent implements OnInit {
     private cookies: CookieService
   ) {
     this.forgotForm = fb.group({
-      email: [this.cookies.get('rememberMe'), [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
     }, { updateOn: 'blur' });
 
     this.forgotForm.statusChanges.subscribe(() => this.setErrorMessages());
