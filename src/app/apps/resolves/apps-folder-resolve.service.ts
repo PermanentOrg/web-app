be490ea87bbd6221150221ea4df79b5565e41cdf
Andrew Atwood <sandurz@gmail.com>
2018-08-23 17:14:26 -0500

    add error catch redirector for myfiles/apps

--- src/app/apps/resolves/apps-folder-resolve.service.ts
+++ src/app/apps/resolves/apps-folder-resolve.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import * as _ from 'lodash';
@@ -7,6 +7,7 @@ import * as _ from 'lodash';
 import { ApiService } from '@shared/services/api/api.service';
 import { AccountService } from '@shared/services/account/account.service';
 import { DataService } from '@shared/services/data/data.service';
+import { MessageService } from '@shared/services/message/message.service';
 
 import { FolderVO } from '@root/app/models';
 
@@ -15,12 +16,23 @@ import { FolderResponse } from '@shared/services/api/index.repo';
 @Injectable()
 export class AppsFolderResolveService implements Resolve<any> {
 
-  constructor(private api: ApiService, private accountService: AccountService, private dataService: DataService) { }
+  constructor(
+    private api: ApiService,
+    private accountService: AccountService,
+    private dataService: DataService,
+    private message: MessageService,
+    private router: Router
+  ) { }
 
   resolve( route: ActivatedRouteSnapshot, state: RouterStateSnapshot ): Observable<any>|Promise<any> {
     return this.loadAppsFolder()
       .then((appsFolder: FolderVO) => {
         return this.loadConnectorFolders(appsFolder);
+      })
+      .catch((response: FolderResponse) => {
+        this.message.showError(response.getMessage(), true);
+        this.router.navigate(['/']);
+        return Promise.reject(false);
       });
   }
 
