891c8bae007ce1c274a36446b9aad3c5d136db5f
Andrew Atwood <sandurz@gmail.com>
2018-08-24 14:21:02 -0500

    fix tests

--- src/app/core/services/upload/upload.service.spec.ts
+++ src/app/core/services/upload/upload.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, inject } from '@angular/core/testing';
+import * as Testing from '@root/test/testbedConfig';
 
 import { UploadService } from '@core/services/upload/upload.service';
 
 describe('UploadService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [UploadService]
-    });
+    const config = Testing.BASE_TEST_CONFIG;
+    const providers = config.providers as any[];
+    providers.push(UploadService);
+    TestBed.configureTestingModule(config);
   });
 
   it('should be created', inject([UploadService], (service: UploadService) => {
