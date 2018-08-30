b8b9bac493598da9ffb7f56c799e90af5c0006c5
Andrew Atwood <sandurz@gmail.com>
2018-08-30 11:19:27 -0500

    fade in video player when loaded

--- src/app/shared/components/video/video.component.ts
+++ src/app/shared/components/video/video.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, Renderer } from '@angular/core';
+import { TweenMax } from 'gsap';
 
 import { RecordVO } from '@root/app/models';
+
+const FADE_IN_DURATION = 0.3;
 
 @Component({
   selector: 'pr-video',
@@ -10,11 +13,26 @@ import { RecordVO } from '@root/app/models';
 export class VideoComponent implements OnInit {
   @Input() item: RecordVO;
 
+  private videoElem: Element;
   public videoSrc: string;
 
-  constructor() { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer) { }
 
   ngOnInit() {
+    this.videoElem = this.elementRef.nativeElement.querySelector('video');
+
+    this.videoElem.addEventListener('canplay', (event) => {
+      this.renderer.setElementClass(this.videoElem, 'loading', false);
+      TweenMax.from(
+        this.videoElem,
+        FADE_IN_DURATION,
+        {
+          opacity: 0,
+          ease: 'Power4.easeOut'
+        }
+      );
+    });
+
     const fileVO = this.item.FileVOs[0];
     this.videoSrc = fileVO.fileURL;
   }
