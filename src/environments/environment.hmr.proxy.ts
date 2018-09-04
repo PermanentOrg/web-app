ab8cbd7ef6d88d6b4f62a6bfff0e769d31b976e0
Andrew Atwood <aatwood@permanent.org>
2018-09-04 12:00:38 -0500

    setup local dev against dev server

---
+++ src/environments/environment.hmr.proxy.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiKey: 'wlK?anNl,BW%W#5;FJN11qGn',
+  apiUrl: 'https://local.permanent.org:4200/api',
+  uploaderUrl: 'https://local.permanent.org:4200/ws',
+  hmr: true,
+};
