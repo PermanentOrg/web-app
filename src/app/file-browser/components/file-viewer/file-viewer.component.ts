b3738618a1096981b693f215490736f5b1d35b16
Andrew Atwood <aatwood@permanent.org>
2018-09-05 13:11:04 -0500

    test hammer events

--- src/app/file-browser/components/file-viewer/file-viewer.component.ts
+++ src/app/file-browser/components/file-viewer/file-viewer.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit, OnDestroy, ElementRef, Inject} from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef, Inject, AfterViewInit, Renderer, Renderer2} from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { Router, ActivatedRoute } from '@angular/router';
+
+import * as Hammer from 'hammerjs';
+import { TweenMax } from 'gsap';
+
 import { RecordVO } from '@root/app/models';
 
 @Component({
@@ -8,16 +12,27 @@ import { RecordVO } from '@root/app/models';
   templateUrl: './file-viewer.component.html',
   styleUrls: ['./file-viewer.component.scss']
 })
-export class FileViewerComponent implements OnInit, OnDestroy {
+export class FileViewerComponent implements OnInit, AfterViewInit, OnDestroy {
   record: RecordVO;
 
-  private viewerElement: Element;
+  private viewerElement: HTMLElement;
+  private thumbElement: HTMLElement;
   private bodyScroll: number;
+  private hammer: HammerManager;
+
+  private velocityThreshold = 0.2;
+  private offscreenThreshold: number;
 
   public showThumbnail = true;
   public isVideo = false;
 
-  constructor(private router: Router, private route: ActivatedRoute, private element: ElementRef, @Inject(DOCUMENT) private document: any) {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private element: ElementRef,
+    @Inject(DOCUMENT) private document: any,
+    private renderer: Renderer2
+  ) {
     this.record = route.snapshot.data.currentRecord;
   }
 
@@ -28,8 +43,54 @@ export class FileViewerComponent implements OnInit, OnDestroy {
     this.isVideo = this.record.type.includes('video');
   }
 
+  ngAfterViewInit() {
+    this.thumbElement = this.element.nativeElement.querySelector('#main-thumb') as HTMLElement;
+
+    this.hammer = new Hammer(this.thumbElement);
+
+    this.offscreenThreshold = this.thumbElement.clientWidth / 2;
+
+    this.hammer.on('pan', (evt: HammerInput) => {
+      this.handlePanEvent(evt);
+    });
+  }
+
   ngOnDestroy() {
     this.document.body.style.setProperty('overflow', '');
+  }
+
+  handlePanEvent(evt: HammerInput) {
+    if (!evt.isFinal) {
+      this.renderer.setStyle(this.thumbElement, 'transform', `translateX(${evt.deltaX}px)`);
+    } else if (Math.abs(evt.velocityX) <= this.velocityThreshold && Math.abs(evt.deltaX) <= this.offscreenThreshold) {
+      TweenMax.fromTo(
+        this.thumbElement,
+        0.5,
+        {
+          x: evt.deltaX
+        },
+        {
+          x: 0,
+          ease: 'Power4.easeOut'
+        } as any
+      );
+    } else {
+      let offscreenX = this.thumbElement.clientWidth + 10;
+      if (evt.deltaX < 0) {
+        offscreenX = -1 * offscreenX;
+      }
+      TweenMax.fromTo(
+        this.thumbElement,
+        0.5,
+        {
+          x: evt.deltaX
+        },
+        {
+          x: offscreenX,
+          ease: 'Power4.easeOut'
+        }
+      );
+    }
   }
 
   close() {
