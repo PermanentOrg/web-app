30ce359bc452b84c5d0a094d4130abac95103954
Andrew Atwood <aatwood@permanent.org>
2018-09-18 15:41:01 -0500

    fix broken tests

--- src/app/shares/components/share-by-me/share-by-me.component.spec.ts
+++ src/app/shares/components/share-by-me/share-by-me.component.spec.ts
@@ -1,19 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import * as Testing from '@root/test/testbedConfig';
+import { cloneDeep } from 'lodash';
 
 import { ShareByMeComponent } from './share-by-me.component';
+import { SharedModule } from '@shared/shared.module';
+import { FileBrowserModule } from '@fileBrowser/file-browser.module';
+import { DataService } from '@shared/services/data/data.service';
+import { AccountService } from '@shared/services/account/account.service';
+import { TEST_DATA } from '@core/core.module.spec';
 
 describe('ShareByMeComponent', () => {
   let component: ShareByMeComponent;
   let fixture: ComponentFixture<ShareByMeComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ShareByMeComponent ]
-    })
+    const config = cloneDeep(Testing.BASE_TEST_CONFIG);
+    config.declarations.push(ShareByMeComponent);
+
+    config.providers.push(DataService);
+    config.imports.push(SharedModule);
+    config.imports.push(FileBrowserModule);
+
+    TestBed.configureTestingModule(config)
     .compileComponents();
   }));
 
   beforeEach(() => {
+    const accountService = TestBed.get(AccountService) as AccountService;
+    const currentArchive = TEST_DATA.archive;
+    accountService.setArchive(currentArchive);
+
     fixture = TestBed.createComponent(ShareByMeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/shares/components/share-with-me/share-with-me.component.spec.ts
+++ src/app/shares/components/share-with-me/share-with-me.component.spec.ts
@@ -1,21 +1,61 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import * as Testing from '@root/test/testbedConfig';
+import { cloneDeep } from 'lodash';
 
 import { ShareWithMeComponent } from './share-with-me.component';
+import { SharedModule } from '@shared/shared.module';
+import { FileBrowserModule } from '@fileBrowser/file-browser.module';
+import { DataService } from '@shared/services/data/data.service';
+import { AccountService } from '@shared/services/account/account.service';
+import { TEST_DATA, TEST_DATA_2 } from '@core/core.module.spec';
+import { ShareComponent } from '@shares/components/share/share.component';
+import { ActivatedRoute } from '@angular/router';
+import { ShareResponse } from '@shared/services/api/share.repo';
+import { ArchiveVO } from '@root/app/models';
+
+const getSharesData = require('@root/test/responses/share.getShares.success.json');
+
+const shares = new ShareResponse(getSharesData).getShareArchiveVOs();
+const currentArchive = new ArchiveVO({
+  archiveId: 8552,
+  fullName: 'John Smith'
+});
 
 describe('ShareWithMeComponent', () => {
   let component: ShareWithMeComponent;
   let fixture: ComponentFixture<ShareWithMeComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ShareWithMeComponent ]
-    })
+    const config = cloneDeep(Testing.BASE_TEST_CONFIG);
+    config.declarations.push(ShareWithMeComponent);
+    config.declarations.push(ShareComponent);
+
+    config.providers.push(DataService);
+    config.imports.push(SharedModule);
+    config.imports.push(FileBrowserModule);
+
+    config.providers.push({
+      provide: ActivatedRoute,
+      useValue: {
+        snapshot: {
+          data: {
+            shares: shares
+          }
+        }
+      }
+    });
+
+    TestBed.configureTestingModule(config)
     .compileComponents();
   }));
 
   beforeEach(() => {
+    const accountService = TestBed.get(AccountService) as AccountService;
+    accountService.setArchive(currentArchive);
+
     fixture = TestBed.createComponent(ShareWithMeComponent);
-    component = fixture.componentInstance;
+    component = fixture.componentInstance as ShareWithMeComponent;
     fixture.detectChanges();
   });
 
