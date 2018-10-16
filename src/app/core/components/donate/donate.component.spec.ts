4165423eaf95ae2edf76846ed4c89daa824b5842
Andrew Atwood <aatwood@permanent.org>
2018-10-16 15:45:44 -0500

    wip

--- src/app/core/components/donate/donate.component.spec.ts
+++ src/app/core/components/donate/donate.component.spec.ts
@@ -1,15 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import * as Testing from '@root/test/testbedConfig';
+import { cloneDeep  } from 'lodash';
 
 import { DonateComponent } from './donate.component';
+import { SharedModule } from '@shared/shared.module';
 
-describe('DonateComponent', () => {
+fdescribe('DonateComponent', () => {
   let component: DonateComponent;
   let fixture: ComponentFixture<DonateComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DonateComponent ]
-    })
+    const config = cloneDeep(Testing.BASE_TEST_CONFIG);
+
+    config.imports.push(SharedModule);
+    config.declarations.push(DonateComponent);
+
+    TestBed.configureTestingModule(config)
     .compileComponents();
   }));
 
--- src/app/core/components/donate/donate.component.ts
+++ src/app/core/components/donate/donate.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+enum DonationStage {
+  Init,
+  Confirm,
+  Complete
+}
+
 @Component({
   selector: 'pr-donate',
   templateUrl: './donate.component.html',
   styleUrls: ['./donate.component.scss']
 })
 export class DonateComponent implements OnInit {
+  public donationStage: DonationStage = DonationStage.Init;
 
   constructor() { }
 
