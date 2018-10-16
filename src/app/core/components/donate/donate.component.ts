9c9b1f34cbf582dbb6cae4b7b43d8c0bba28217f
Andrew Atwood <aatwood@permanent.org>
2018-10-16 17:05:34 -0500

    stub out separate screens

--- src/app/core/components/donate/donate.component.ts
+++ src/app/core/components/donate/donate.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '@shared/services/api/api.service';
+import { AccountService } from '@shared/services/account/account.service';
 
 enum DonationStage {
-  Init,
+  Storage,
+  Permanent,
+  ByteForByte,
   Confirm,
   Complete
 }
@@ -12,11 +17,26 @@ enum DonationStage {
   styleUrls: ['./donate.component.scss']
 })
 export class DonateComponent implements OnInit {
-  public donationStage: DonationStage = DonationStage.Init;
+  public donationStage: DonationStage = DonationStage.Storage;
+  public donationForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    fb: FormBuilder,
+    api: ApiService,
+    accountService: AccountService
+  ) {
+    this.donationForm = fb.group({
+      storageAmount: ['10', [Validators.required]],
+      customAmount: [''],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  setStorageAmount(amount: string) {
+    this.donationForm.controls['storageAmount'].setValue(amount);
+    console.log(this.donationForm.value);
   }
 
 }
