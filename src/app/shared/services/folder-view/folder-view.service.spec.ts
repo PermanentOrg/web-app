7817f1a72babce3f527e722bed934a92460cda9c
Andrew Atwood <aatwood@permanent.org>
2018-11-14 17:38:47 -0600

    update unit tests

--- src/app/shared/services/folder-view/folder-view.service.spec.ts
+++ src/app/shared/services/folder-view/folder-view.service.spec.ts
@@ -3,16 +3,26 @@ import * as Testing from '@root/test/testbedConfig';
 import { cloneDeep  } from 'lodash';
 
 import { FolderViewService } from '@shared/services/folder-view/folder-view.service';
+import { FolderView } from './folder-view.enum';
 
 describe('FolderViewService', () => {
   beforeEach(() => {
     const config = cloneDeep(Testing.BASE_TEST_CONFIG);
     const providers = config.providers;
-    providers.push(FolderViewService);
     TestBed.configureTestingModule(config);
   });
 
   it('should be created', inject([FolderViewService], (service: FolderViewService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should emit folder view changes', inject([FolderViewService], (service: FolderViewService) => {
+    const newFolderView = FolderView.Grid;
+
+    service.viewChange.subscribe((folderViewEmitted: FolderView) => {
+      expect(folderViewEmitted).toEqual(newFolderView);
+    });
+
+    service.setFolderView(newFolderView);
   }));
 });
