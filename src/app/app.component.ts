2d1b7080c1098f2c3dc5c196669494f8f077aac4
Andrew Atwood <aatwood@permanent.org>
2018-12-09 09:42:15 -0600

    add iosinnerheight package, check for menu bar showing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { ApiService } from '@shared/services/api/api.service';
 import { AccountService } from '@shared/services/account/account.service';
 import { MessageService } from '@shared/services/message/message.service';
 
+declare var iosInnerHeight: Function;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,11 +12,20 @@ import { MessageService } from '@shared/services/message/message.service';
 })
 export class AppComponent implements OnInit {
   @HostBinding('class.mobile-safari') isMobileSafari = false;
+  @HostBinding('class.mobile-safari-menu-bar-showing') isMenuBarShowing = false;
 
   constructor(private api: ApiService, private account: AccountService, private message: MessageService) {
     const isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
     const iPhone = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window['MSStream'];
     this.isMobileSafari = isSafari && iPhone;
+    this.isMenuBarShowing = window.innerHeight !== iosInnerHeight();
+
+    if (this.isMobileSafari) {
+      window.addEventListener('resize', (ev) => {
+        this.isMenuBarShowing = window.innerHeight !== iosInnerHeight();
+      });
+    }
+
   }
 
   ngOnInit() {
