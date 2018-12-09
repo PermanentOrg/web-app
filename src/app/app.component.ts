80125c669797593f1001c1e86b589ada2d77c8bc
Andrew Atwood <aatwood@permanent.org>
2018-12-09 09:17:06 -0600

    add mobile safari class for padding bottom only as needed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { ApiService } from '@shared/services/api/api.service';
 import { AccountService } from '@shared/services/account/account.service';
 import { MessageService } from '@shared/services/message/message.service';
@@ -9,7 +9,12 @@ import { MessageService } from '@shared/services/message/message.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  @HostBinding('class.mobile-safari') isMobileSafari = false;
+
   constructor(private api: ApiService, private account: AccountService, private message: MessageService) {
+    const isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
+    const iPhone = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window['MSStream'];
+    this.isMobileSafari = isSafari && iPhone;
   }
 
   ngOnInit() {
