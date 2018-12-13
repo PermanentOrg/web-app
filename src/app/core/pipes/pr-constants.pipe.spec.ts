7471866b412f5c03ec8fca358e7582a902c1b52d
Andrew Atwood <aatwood@permanent.org>
2018-12-13 12:45:26 -0600

    fix pipe test

--- src/app/core/pipes/pr-constants.pipe.spec.ts
+++ src/app/core/pipes/pr-constants.pipe.spec.ts
@@ -1,8 +1,11 @@
 import { PrConstantsPipe } from './pr-constants.pipe';
+import { PrConstantsService } from '@shared/services/pr-constants/pr-constants.service';
+
+const prConstants = new PrConstantsService();
 
 describe('PrConstantsPipe', () => {
   it('create an instance', () => {
-    const pipe = new PrConstantsPipe();
+    const pipe = new PrConstantsPipe(prConstants);
     expect(pipe).toBeTruthy();
   });
 });
