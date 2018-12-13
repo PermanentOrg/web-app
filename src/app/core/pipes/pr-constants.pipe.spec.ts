13758443644dda2e5b54be0165794a8ea36f54c3
Andrew Atwood <aatwood@permanent.org>
2018-12-13 12:48:15 -0600

    add actual tests lol

--- src/app/core/pipes/pr-constants.pipe.spec.ts
+++ src/app/core/pipes/pr-constants.pipe.spec.ts
@@ -8,4 +8,16 @@ describe('PrConstantsPipe', () => {
     const pipe = new PrConstantsPipe(prConstants);
     expect(pipe).toBeTruthy();
   });
+
+  it('translates an existing string', () => {
+    const pipe = new PrConstantsPipe(prConstants);
+    const constantString = 'access.role.viewer';
+    expect(pipe.transform(constantString)).toEqual('Viewer');
+  });
+
+  it('returns string unchanged if missing', () => {
+    const pipe = new PrConstantsPipe(prConstants);
+    const constantString = 'access.role.missing';
+    expect(pipe.transform(constantString)).toEqual(constantString);
+  });
 });
