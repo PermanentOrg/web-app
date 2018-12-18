894d5f55b030c506b000f887e452361ec94facd4
Andrew Atwood <aatwood@permanent.org>
2018-12-18 14:40:38 -0600

    specify token for dialog components, fix uncaught promise catch in add relation

--- src/app/dialog/dialog.module.ts
+++ src/app/dialog/dialog.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, ModuleWithProviders, ComponentFactoryResolver } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Dialog, DialogRef, DIALOG_DATA } from './dialog.service';
+import { Dialog, DialogRef, DIALOG_DATA, DialogChildComponentData } from './dialog.service';
 import { DialogComponent } from './dialog.component';
 import { DialogRootComponent } from './dialog-root.component';
 
-export { Dialog, DialogRef, DialogRootComponent, DIALOG_DATA };
+export { Dialog, DialogRef, DialogRootComponent, DIALOG_DATA, DialogChildComponentData };
 
 @NgModule({
   imports: [
--- src/app/file-browser/file-browser.module.ts
+++ src/app/file-browser/file-browser.module.ts
@@ -11,7 +11,7 @@ import { FileViewerComponent } from '@fileBrowser/components/file-viewer/file-vi
 import { ThumbnailComponent } from '@shared/components/thumbnail/thumbnail.component';
 import { VideoComponent } from '@shared/components/video/video.component';
 import { SharingComponent } from '@fileBrowser/components/sharing/sharing.component';
-import { Dialog } from '../dialog/dialog.service';
+import { Dialog, DialogChildComponentData } from '../dialog/dialog.service';
 import { DialogModule } from '../dialog/dialog.module';
 import { ArchivePickerComponent } from '@shared/components/archive-picker/archive-picker.component';
 
@@ -43,9 +43,10 @@ import { ArchivePickerComponent } from '@shared/components/archive-picker/archiv
   ]
 })
 export class FileBrowserModule {
-  private dialogComponents = [
-    SharingComponent
-  ];
+  private dialogComponents: DialogChildComponentData[] = [{
+    token: 'SharingComponent',
+    component: SharingComponent
+  }];
 
   constructor(private dialog: Dialog, resolver: ComponentFactoryResolver) {
     this.dialog.registerComponents(this.dialogComponents, resolver, true);
