9969abee50192cb683425bf8a40b669a6c69d2b8
Andrew Atwood <aatwood@permanent.org>
2019-01-21 18:33:17 -0600

    add progresbar.js

--- src/app/pledge/components/phase-progress/phase-progress.component.ts
+++ src/app/pledge/components/phase-progress/phase-progress.component.ts
@@ -1,34 +1,49 @@
 import { Component, OnInit, ElementRef, ContentChild } from '@angular/core';
-import { TweenLite, TweenMax } from 'gsap';
+import ProgressBar from 'progressbar.js';
+
 @Component({
   selector: 'pr-phase-progress',
   templateUrl: './phase-progress.component.html',
   styleUrls: ['./phase-progress.component.scss']
 })
 export class PhaseProgressComponent implements OnInit {
-  public progress = .75;
-  public innerBar: HTMLElement;
+  public progress = .45;
+  public innerBar: ProgressBar;
 
   constructor(
     private elementRef: ElementRef
   ) { }
 
   ngOnInit() {
-    this.innerBar = this.elementRef.nativeElement.querySelector('.progress-bar-inner');
+    this.innerBar = new ProgressBar.Line(this.elementRef.nativeElement.querySelector('.progress-bar'), {
+      strokeWidth: 4,
+      easing: 'easeInOut',
+      duration: 1400,
+      color: '#800080',
+      svgStyle: {width: '100%', height: '100%'},
+      from: {color: '#FFEA82'},
+      to: {color: '#ED6A5A'}
+    });
     setTimeout(() => {
       this.redrawProgress();
     });
+
+    setTimeout(() => {
+      this.progress = 0.55;
+      this.redrawProgress();
+    }, 4000);
   }
 
   redrawProgress() {
-    TweenLite.to(
-      this.innerBar,
-      2,
-      {
-        width: `${this.progress * 100}%`,
-        ease: 'Power4.easeOut'
-      }
-    );
+    this.innerBar.animate(this.progress);
+    // TweenLite.to(
+    //   this.innerBar,
+    //   2,
+    //   {
+    //     width: `${this.progress * 100}%`,
+    //     ease: 'Power4.easeOut'
+    //   }
+    // );
   }
 
 }
