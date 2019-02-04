a443100b226537753380b7b69def3a1862c70977
Andrew Atwood <aatwood@permanent.org>
2019-02-04 12:23:14 -0600

    stub login and mfa embed components

---
+++ src/app/embed/components/login-embed/login-embed.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import APP_CONFIG from '@root/app/app.config';
+import { matchControlValidator } from '@shared/utilities/forms';
+
+import { AccountService } from '@shared/services/account/account.service';
+import { AuthResponse } from '@shared/services/api/auth.repo';
+import { MessageService } from '@shared/services/message/message.service';
+import { AccountResponse } from '@shared/services/api/index.repo';
+
+import * as FormUtilities from '@shared/utilities/forms';
+
+const MIN_PASSWORD_LENGTH = APP_CONFIG.passwordMinLength;
+
+@Component({
+  selector: 'pr-signup',
+  templateUrl: './signup-embed.component.html',
+  styleUrls: ['./signup-embed.component.scss'],
+  host: {'class': 'pr-auth-form'}
+})
+export class SignupEmbedComponent implements OnInit {
+  signupForm: FormGroup;
+  waiting: boolean;
+  inviteCode: string;
+
+  formErrors = {
+    name: false,
+    invitation: false,
+    email: false,
+    password: false,
+    confirm: false
+  };
+
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private message: MessageService
+  ) {
+    const queryParams = this.route.snapshot.queryParams;
+    if (queryParams.invite) {
+      this.inviteCode = queryParams.invite;
+    }
+
+    this.signupForm = fb.group({
+      invitation: [this.inviteCode ? this.inviteCode : '', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(MIN_PASSWORD_LENGTH)]],
+      agreed: [false, [Validators.requiredTrue]],
+      optIn: [true]
+    });
+
+    const confirmPasswordControl = new FormControl('',
+    [
+      Validators.required,
+      matchControlValidator(this.signupForm.controls['password'])
+    ]);
+    this.signupForm.addControl('confirm', confirmPasswordControl);
+  }
+
+  ngOnInit() {
+    const currentAccount = this.accountService.getAccount();
+    if (currentAccount && currentAccount.primaryEmail) {
+      this.router.navigate(['/embed', 'done'], {queryParams: { existing: true, inviteCode: this.inviteCode }});
+    }
+  }
+
+  onSubmit(formValue: any) {
+    this.waiting = true;
+
+    this.accountService.signUp(
+      formValue.email, formValue.name, formValue.password, formValue.confirm,
+      formValue.agreed, formValue.optIn, null, formValue.invitation
+    ).then((response: AccountResponse) => {
+        const account = response.getAccountVO();
+        if (account.needsVerification()) {
+          this.router.navigate(['/embed', 'verify']);
+        } else {
+          this.accountService.logIn(formValue.email, formValue.password, true, true)
+          .then(() => {
+            this.router.navigate(['/embed', 'done'], {queryParams: { inviteCode: this.inviteCode }});
+          });
+        }
+      })
+      .catch((response: AccountResponse) => {
+        this.message.showError(response.getMessage(), true);
+        this.waiting = false;
+      });
+  }
+
+}
---
+++ src/app/embed/components/mfa-embed/mfa-embed.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AccountService } from '@shared/services/account/account.service';
+import { MessageService } from '@shared/services/message/message.service';
+import { AuthResponse, ArchiveResponse, AccountResponse } from '@shared/services/api/index.repo';
+
+@Component({
+  selector: 'pr-verify',
+  templateUrl: './verify-embed.component.html',
+  styleUrls: ['./verify-embed.component.scss']
+})
+export class VerifyEmbedComponent implements OnInit {
+  verifyForm: FormGroup;
+  waiting: boolean;
+
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router, private message: MessageService) {
+    this.verifyForm = fb.group({
+      'token': ['', Validators.required],
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(formValue: any) {
+    this.waiting = true;
+
+    this.accountService.verifyEmail(formValue.token)
+      .then(() => {
+        return this.accountService.switchToDefaultArchive();
+      })
+      .then((response: ArchiveResponse) => {
+        this.waiting = false;
+        this.router.navigate(['/embed', 'done']);
+      })
+      .catch((response: ArchiveResponse | AccountResponse) => {
+        this.waiting = false;
+        this.message.showError(response.getMessage(), true);
+      });
+  }
+
+}
