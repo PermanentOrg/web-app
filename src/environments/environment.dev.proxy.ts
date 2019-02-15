e0130f36f9b3fa06f100187a084104a48b52f507
Andrew Atwood <aatwood@permanent.org>
2019-02-15 14:26:04 -0600

    pull stripeKey

--- src/environments/environment.dev.proxy.ts
+++ src/environments/environment.dev.proxy.ts
@@ -3,5 +3,6 @@ export const environment = {
   apiKey: 'wlK?anNl,BW%W#5;FJN11qGn',
   apiUrl: 'https://local.permanent.org:4200/api',
   uploaderUrl: 'https://local.permanent.org:4200/ws',
+  stripeKey: 'pk_test_kGSsLxH88lyxBUp9Lluji2Rn',
   hmr: false,
 };
