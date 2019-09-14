92a7d2071082cc350fe9793a0b1369d89538d59f
Andrew Atwood <aatwood@permanent.org>
2019-09-14 08:34:54 -0500

    redirect to /app if not mobile and shareUrl already used

--- src/app/share-preview/resolves/share-url-resolve.service.ts
+++ src/app/share-preview/resolves/share-url-resolve.service.ts
@@ -3,6 +3,7 @@ import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@a
 
 import { ApiService } from '@shared/services/api/api.service';
 import { MessageService } from '@shared/services/message/message.service';
+import { DeviceService } from '@shared/services/device/device.service';
 
 import { ArchiveResponse, ShareResponse } from '@shared/services/api/index.repo';
 import { RecordVO, ArchiveVO, FolderVO } from '@models/index';
@@ -12,7 +13,8 @@ export class ShareUrlResolveService implements Resolve<any> {
   constructor(
     private api: ApiService,
     private message: MessageService,
-    private router: Router
+    private router: Router,
+    private device: DeviceService
   ) { }
 
   resolve( route: ActivatedRouteSnapshot, state: RouterStateSnapshot ) {
@@ -22,14 +24,21 @@ export class ShareUrlResolveService implements Resolve<any> {
           const shareByUrlVO = response.getShareByUrlVO();
           const shareVO = shareByUrlVO.ShareVO;
 
-          // need to navigate to /app to handle /app and /m redirects automatically
           if (!shareVO || shareVO.status.includes('pending')) {
             return shareByUrlVO;
           } else if (shareVO.status.includes('ok') && shareByUrlVO.RecordVO) {
-            return this.router.navigate(['/shares', 'withme']);
+            if (this.device.isMobile()) {
+              return this.router.navigate(['/shares', 'withme']);
+            } else {
+              window.location.assign(`/app/shares/`);
+            }
           } else if (shareVO.status.includes('ok')) {
             const folder: FolderVO = shareByUrlVO.FolderVO;
-            return this.router.navigate(['/shares', 'withme', folder.archiveNbr, folder.folder_linkId]);
+            if (this.device.isMobile()) {
+              return this.router.navigate(['/shares', 'withme', folder.archiveNbr, folder.folder_linkId]);
+            } else {
+              window.location.assign(`/app/shares/${folder.archiveNbr}/${folder.folder_linkId}`);
+            }
           }
         } else {
           throw response;
