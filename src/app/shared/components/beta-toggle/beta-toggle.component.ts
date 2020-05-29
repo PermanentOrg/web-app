65b4e5cc5d274db9ec6b6f87eb5f19de60ecbd41
Andrew Atwood <aatwood@permanent.org>
2020-05-29 09:10:44 -0500

    redirect after hitting button, only display if cookie exists

--- src/app/shared/components/beta-toggle/beta-toggle.component.ts
+++ src/app/shared/components/beta-toggle/beta-toggle.component.ts
@@ -7,15 +7,21 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./beta-toggle.component.scss']
 })
 export class BetaToggleComponent implements OnInit {
+  public hasCookie: boolean;
+
   constructor(
     private cookie: CookieService
-  ) { }
+  ) {
+    this.hasCookie = this.cookie.check('permBeta');
+  }
 
   ngOnInit(): void {
   }
 
   onClick() {
     this.cookie.delete('permBeta', '/', `.${window.location.hostname}`);
-    console.log(this.cookie.get('permBeta'));
+    setTimeout(() => {
+      window.location.assign(`https://${window.location.host}/app`);
+    });
   }
 }
