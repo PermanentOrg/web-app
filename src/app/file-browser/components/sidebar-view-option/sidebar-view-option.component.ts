439f4ea67d4761a2bfde2cb6de14156537066306
Andrew Atwood <aatwood@permanent.org>
2020-06-09 07:48:33 -0500

    add view icons, wire up folder default checklists

--- src/app/file-browser/components/sidebar-view-option/sidebar-view-option.component.ts
+++ src/app/file-browser/components/sidebar-view-option/sidebar-view-option.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FolderViewType, ItemVO, FolderVO } from '@models';
 import { Router } from '@angular/router';
 import { environment } from '@root/environments/environment';
 
+type FolderViewImages = {
+  [Type in FolderViewType]?: string;
+};
 
 @Component({
   selector: 'pr-sidebar-view-option',
@@ -12,10 +15,37 @@ import { environment } from '@root/environments/environment';
 export class SidebarViewOptionComponent implements OnInit {
   @Input() folderView: FolderViewType;
   @Input() folder: FolderVO;
+  @Input() folderDefault: FolderViewType;
+
+  @Output() setDefault = new EventEmitter<FolderViewType>();
+
+  get isDefault(): boolean {
+    return this.folderDefault === this.folderView;
+  }
+
+  set isDefault(value: boolean) {
+    if (value) {
+      this.onSetDefault();
+    }
+  }
+
+  images: FolderViewImages = {
+    'folder.view.grid': 'assets/img/views/grid.png',
+    'folder.view.timeline': 'assets/img/views/timeline.png'
+  };
+
   constructor(
     private router: Router
   ) { }
 
   ngOnInit(): void {
+  }
+
+  onSetDefault() {
+    if (this.folderDefault === this.folderView) {
+      return;
+    } else {
+      this.setDefault.emit(this.folderView);
+    }
   }
 }
