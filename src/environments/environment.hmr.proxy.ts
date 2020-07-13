589f239787399f5d7c7da6931aa7818df86f3e8f
Andrew Atwood <aatwood@permanent.org>
2020-07-13 14:59:29 -0500

    Separate out proxy + proxy_hmr configs

--- src/environments/environment.hmr.proxy.ts
+++ src/environments/environment.hmr.proxy.ts
@@ -5,7 +5,7 @@ export const environment = {
   apiKey: 'wlK?anNl,BW%W#5;FJN11qGn',
   apiUrl: 'https://local.permanent.org:4200/api',
   uploaderUrl: 'wss://local.permanent.org:4200/uploadsvc',
-  hmr: false,
+  hmr: true,
   stripeKey: 'pk_test_kGSsLxH88lyxBUp9Lluji2Rn',
   firebase: {
     apiKey: 'AIzaSyAThS43euMcSjPdDvtB9qM-9-diloP-eHE',
