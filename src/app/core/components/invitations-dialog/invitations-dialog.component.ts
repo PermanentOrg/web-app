c8a4050827fb183cab87ae7a593a50ec1e1faf51
Andrew Atwood <aatwood@permanent.org>
2020-08-12 15:56:05 -0500

    Wire up invitation send in invitation dialog

--- src/app/core/components/invitations-dialog/invitations-dialog.component.ts
+++ src/app/core/components/invitations-dialog/invitations-dialog.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { IsTabbedDialog } from '@root/app/dialog/dialog.module';
+import { Component, OnInit, Inject } from '@angular/core';
+import { IsTabbedDialog, DIALOG_DATA, DialogRef } from '@root/app/dialog/dialog.module';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '@shared/services/api/api.service';
+import { InviteVOData, InviteVO } from '@models';
+import { InviteResponse } from '@shared/services/api/index.repo';
+import { MessageService } from '@shared/services/message/message.service';
 
 type InvitationsTab = 'new' | 'pending' | 'accepted';
 
@@ -11,13 +15,18 @@ type InvitationsTab = 'new' | 'pending' | 'accepted';
 })
 export class InvitationsDialogComponent implements OnInit, IsTabbedDialog {
   newInviteForm: FormGroup;
+  waiting = false;
 
   activeTab: InvitationsTab = 'new';
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    @Inject(DIALOG_DATA) public data: any,
+    private dialogRef: DialogRef,
+    private api: ApiService,
+    private messageService: MessageService
   ) {
     this.newInviteForm = this.fb.group({
-      name: ['', [Validators.required]],
+      fullName: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]]
     });
   }
@@ -30,6 +39,26 @@ export class InvitationsDialogComponent implements OnInit, IsTabbedDialog {
   }
 
   onDoneClick() {
+    this.dialogRef.close();
+  }
+
+  async onNewInviteFormSubmit(value: InviteVOData) {
+    try {
+      this.waiting = true;
+      value.relationship = 'relation.friend';
+      const newInvite = new InviteVO(value);
+      await this.api.invite.send([newInvite]);
+      const message = `Invitation to ${newInvite.email} successfully sent`;
+      this.messageService.showMessage(message, 'success');
+      this.newInviteForm.reset();
+    } catch (err) {
+      if (err instanceof InviteResponse) {
+        this.messageService.showError(err.getMessage(), true);
+      }
+    } finally {
+      this.waiting = false;
+    }
+
   }
 
 }
