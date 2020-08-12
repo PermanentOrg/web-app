3774657f2c004070200c00f74a507ff02a27af0e
Andrew Atwood <aatwood@permanent.org>
2020-08-12 15:29:12 -0500

    Stub invitation dialog form

--- src/app/core/components/invitations-dialog/invitations-dialog.component.ts
+++ src/app/core/components/invitations-dialog/invitations-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IsTabbedDialog } from '@root/app/dialog/dialog.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 type InvitationsTab = 'new' | 'pending' | 'accepted';
 
@@ -9,11 +10,17 @@ type InvitationsTab = 'new' | 'pending' | 'accepted';
   styleUrls: ['./invitations-dialog.component.scss']
 })
 export class InvitationsDialogComponent implements OnInit, IsTabbedDialog {
+  newInviteForm: FormGroup;
 
   activeTab: InvitationsTab = 'new';
   constructor(
-
-  ) { }
+    private fb: FormBuilder
+  ) {
+    this.newInviteForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
 
   ngOnInit(): void {
   }
