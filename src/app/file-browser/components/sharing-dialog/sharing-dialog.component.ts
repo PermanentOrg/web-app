822c5d5bd49a5f0a669a717baa93660d881e58ad
Andrew Atwood <aatwood@permanent.org>
2020-09-25 09:53:21 -0500

    Add light blue to theme colors for button variations, wire up share link creation and copy

--- src/app/file-browser/components/sharing-dialog/sharing-dialog.component.ts
+++ src/app/file-browser/components/sharing-dialog/sharing-dialog.component.ts
@@ -3,10 +3,14 @@ import { ActivatedRoute } from '@angular/router';
 import { RelationshipService } from '@core/services/relationship/relationship.service';
 import { RecordVO, FolderVO, ShareVO, ShareByUrlVO, ItemVO } from '@models';
 import { DIALOG_DATA, DialogRef, Dialog } from '@root/app/dialog/dialog.module';
+import { Deferred } from '@root/vendor/deferred';
 import { ApiService } from '@shared/services/api/api.service';
+import { ShareResponse } from '@shared/services/api/share.repo';
+import { EVENTS } from '@shared/services/google-analytics/events';
 import { GoogleAnalyticsService } from '@shared/services/google-analytics/google-analytics.service';
 import { MessageService } from '@shared/services/message/message.service';
-import { PromptService } from '@shared/services/prompt/prompt.service';
+import { DATE_FIELD, NUMBER_FIELD, ON_OFF_FIELD, PromptField, PromptService } from '@shared/services/prompt/prompt.service';
+import { copyFromInputElement } from '@shared/utilities/forms';
 import { partition } from 'lodash';
 
 @Component({
@@ -49,6 +53,93 @@ export class SharingDialogComponent implements OnInit {
 
   onDoneClick(): void {
     this.dialogRef.close();
+  }
+
+
+  async generateShareLink() {
+    const response = await this.api.share.generateShareLink(this.shareItem);
+
+    if (response.isSuccessful) {
+      this.shareLink = response.getShareByUrlVO();
+      this.ga.sendEvent(EVENTS.SHARE.ShareByUrl.initiated.params);
+    }
+  }
+
+  copyShareLink() {
+    const element = this.shareUrlInput.nativeElement as HTMLInputElement;
+
+    copyFromInputElement(element);
+
+    this.linkCopied = true;
+    setTimeout(() => {
+      this.linkCopied = false;
+    }, 5000);
+  }
+
+  manageShareLink() {
+    const deferred = new Deferred();
+    const title = `Manage share link for ${this.shareItem.displayName}`;
+    let currentDate = null;
+    if (this.shareLink.expiresDT) {
+      currentDate = new Date(this.shareLink.expiresDT).toISOString().split('T')[0];
+    }
+    const fields: PromptField[] = [
+      ON_OFF_FIELD('previewToggle', 'Share preview', this.shareLink.previewToggle ? 'on' : 'off'),
+      NUMBER_FIELD('maxUses', 'Max number of uses (optional)', this.shareLink.maxUses, false),
+      DATE_FIELD('expiresDT', 'Expiration date (optional)', currentDate, new Date())
+    ];
+
+    this.promptService.prompt(fields, title, deferred.promise, 'Save')
+    .then(async (result: {previewToggle: 'on' | 'off', expiresDT, maxUses: string}) => {
+      const updatedShareVo = new ShareByUrlVO(this.shareLink);
+      updatedShareVo.previewToggle = result.previewToggle === 'on' ? 1 : 0;
+
+      if (result.maxUses !== undefined) {
+        updatedShareVo.maxUses = parseInt(result.maxUses, 10);
+      }
+
+      if (result.expiresDT) {
+        updatedShareVo.expiresDT = new Date(result.expiresDT).toISOString();
+      }
+
+      try {
+        const updateResponse = await this.api.share.updateShareLink(updatedShareVo);
+        this.shareLink = updateResponse.getShareByUrlVO();
+        deferred.resolve();
+      } catch (response) {
+        deferred.reject();
+        if (response.getMessage()) {
+          this.messageService.showError(response.getMessage());
+        }
+      }
+    })
+    .catch((err) => {
+      if (err instanceof ShareResponse) {
+      } else {
+        console.error(err);
+      }
+    });
+  }
+
+  async removeShareLink() {
+    const deferred = new Deferred();
+    try {
+      await this.promptService.confirm(
+        'Remove link',
+        'Are you sure you want to remove this link?',
+        deferred.promise,
+        'btn-danger'
+      );
+
+      await this.api.share.removeShareLink(this.shareLink);
+      this.shareLink = null;
+      deferred.resolve();
+    } catch (response) {
+      deferred.resolve();
+      if (response instanceof ShareResponse) {
+        this.messageService.showError(response.getMessage());
+      }
+    }
   }
 
 }
