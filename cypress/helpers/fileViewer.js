19e8bab9c6c64ba2bc0cc4891b05a78632aacdff
Andrew Atwood <aatwood@permanent.org>
2020-10-13 10:00:13 -0500

    Upgrade vulnerable deps, remove Cypress and Protractor from deps, remove e2e tests until a future date when they can be run in CI

--- cypress/helpers/fileViewer.js
+++
@@ -1,5 +0,0 @@
-/// <reference types="Cypress" />
-
-export function shouldBeVisible() {
-  cy.get('pr-file-viewer').should('be.visible');
-}
--- cypress/helpers/modal.js
+++
@@ -1,5 +0,0 @@
-/// <reference types="Cypress" />
-
-export function clickModalButton(buttonText) {
-  cy.contains('.dialog-content .btn', buttonText).click();
-}
