78ab3d4310512422b4cfeeffd7f2939c0870409c
Cecilia Krum <cecilia@permanent.org>
2021-01-31 21:25:17 -0600

    WIP: Handle verification clicks
    When the user clicks the verification link in their email, take them
    to the verification page with the code prefilled (instead of
    erroneously redirecting to the login page).  This also requires a
    change in the `api` repo.
    
    Currently this does not work if the user is not logged into Permanent
    (with some account).  Is that what we want?  These edits don't change
    that.
    
    The still-failing part is this case:
    - My account cecilia+testverify2 is unverified
    - I'm logged in as cecilia+local
    - I click the verification link in email for cecilia+testverify2
    - web-app should log me out of cecilia+local and fill out the form for
      cecilia+testverify
    - Currently it fails to create an account var for cecilia+testverify
      and I am going to take a break now instead of looking into why.

--- src/app/auth/components/verify/verify.component.ts
+++ src/app/auth/components/verify/verify.component.ts
@@ -34,27 +34,29 @@ export class VerifyComponent implements OnInit {
 
     const account = this.accountService.getAccount();
 
+    // TODO: Do we even need this?
     if (!account) {
-      this.router.navigate(['/auth', 'login'], { queryParamsHandling: 'preserve'});
+      //this.router.navigate(['/auth', 'login'], { queryParamsHandling: 'preserve'});
       return;
     }
 
-    const params = route.snapshot.params;
-    // if (params.email) {
-    //   const email = window.atob(params.email);
-    //   if (email !== account.primaryEmail) {
-    //     this.accountService.logOut();
-    //     account = new AccountVO(
-    //       {
-    //         primaryEmail: window.atob(params.email),
-    //         emailStatus: 'status.auth.unverified'
-    //       }
-    //     );
-    //     this.accountService.setAccount(account);
-    //   }
-    // }
-
     const queryParams = route.snapshot.queryParams;
+
+    if (queryParams.email) {
+      const email = atob(queryParams.email);
+      if (email !== account.primaryEmail) {
+        console.log("DEBUG: logged into a different account than the email I clicked");
+        this.accountService.logOut();
+        // TODO: this is an invalid assignment
+        account = new AccountVO(
+	  {
+	    primaryEmail: email,
+	    emailStatus: 'status.auth.unverified'
+	  }
+        );
+        this.accountService.setAccount(account);
+      }
+    }
 
     if (queryParams) {
       if (queryParams.sendEmail) {
@@ -78,7 +80,7 @@ export class VerifyComponent implements OnInit {
     }
 
     this.verifyForm = fb.group({
-      'token': [params.code || ''],
+      'token': [queryParams.token || ''],
     });
   }
 
