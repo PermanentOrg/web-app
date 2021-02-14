6e339671c9a39ed6786bc6e54223de8e13b1301e
Andrew Atwood <sandurz@gmail.com>
2021-02-14 09:04:27 -0600

    Create `load-secrets` script
    At app runtime, Angular is run exclusively client-side and has no access to `process.env`, so any secrets must be accessible in a static file available at build time.
    
    To facilitate this, `load-secrets.js` uses `dotenv` to load environment variables from the `.env` file, and in turn writes them to the file `src/secrets.ts`. This file is added to `.gitignore` to prevent any leakage via source control, but is available to the TypeScript compiler during build as a static file containing the secrets from .env in plain Javascript object format.
    
    `load-secrets` must be run prior to any dev server or build to ensure that the `secrets.ts` file is up to date. When running, it checks against `required-secrets` to see if any variables listed as required are missing, and terminates the process if so, which terminates the build step.
    
    `SecretsService` reads from `secrets.ts` and re-validates the available via the `required-secrets` list. Secrets are accessed via the `get` method, which throws an exception if the secret is undefined to minimize downstream errors due to missing secrets.

---
+++ src/app/shared/services/secrets/secrets.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import requiredSecrets from '@root/required-secrets';
+import debug from 'debug';
+
+let secrets = {};
+
+try {
+  secrets = require('@root/secrets').SECRETS;
+} catch (err) {
+  console.error(err);
+  throw new Error('Unable to read secrets.ts - make sure to run `node secrets.js` before building!');
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SecretsService {
+  private debug = debug('service:secretsService');
+
+  constructor() {
+    const missingSecrets = [];
+
+    for (const secret of requiredSecrets) {
+      if (secrets[secret] === undefined) {
+        missingSecrets.push(secret);
+      }
+    }
+
+    this.debug('Loaded %d secrets from secrets.ts', Object.keys(secrets).length);
+
+    if (missingSecrets.length) {
+      console.error('SecretsService: Missing required secrets: %s', missingSecrets.join(','));
+    }
+  }
+
+  get(key: string) {
+    if (secrets[key] === undefined) {
+      throw new Error(`Secret ${key} not found. Check your .env file and restart`);
+    }
+
+    return secrets[key];
+  }
+}
---
+++ src/required-secrets.js
@@ -0,0 +1,8 @@
+const requiredSecrets = [
+  'PERMANENT_API_KEY',
+  'GOOGLE_API_KEY',
+  'STRIPE_API_KEY',
+  'FIREBASE_API_KEY',
+];
+
+module.exports = requiredSecrets;
