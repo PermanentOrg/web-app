dcaf9e7d081399a995db2ec1b947753125ae9f3d
Andrew Atwood <sandurz@gmail.com>
2021-02-22 14:59:13 -0600

    Fix testbed setup for GuidedTourService

--- src/app/shared/services/guided-tour/guided-tour.service.spec.ts
+++ src/app/shared/services/guided-tour/guided-tour.service.spec.ts
@@ -1,12 +1,35 @@
 import { TestBed } from '@angular/core/testing';
+import { cloneDeep } from 'lodash';
+import { AccountService } from '../account/account.service';
+import * as Testing from '@root/test/testbedConfig';
 
 import { GuidedTourService } from './guided-tour.service';
+import { AccountVO } from '@models';
+import { ShepherdService } from 'angular-shepherd';
 
 describe('GuidedTourService', () => {
   let service: GuidedTourService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const config = cloneDeep(Testing.BASE_TEST_CONFIG);
+    const mockAccountService = {
+      getAccount: function() {
+        return new AccountVO({
+          accountId: 2
+        });
+      }
+    };
+
+    config.providers.push({
+      provide: AccountService,
+      useValue: mockAccountService
+    },
+      ShepherdService
+    );
+    config.providers.push(
+      GuidedTourService
+    );
+    TestBed.configureTestingModule(config);
     service = TestBed.inject(GuidedTourService);
   });
 
