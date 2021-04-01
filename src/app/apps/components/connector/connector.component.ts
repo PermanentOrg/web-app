20cc330921483f7baf3b1e356548045f8419aa13
Cecilia Krum <cecilia@permanent.org>
2021-04-01 14:35:55 -0500

    PER-8302: Show some helptext on hover

--- src/app/apps/components/connector/connector.component.ts
+++ src/app/apps/components/connector/connector.component.ts
@@ -68,9 +68,6 @@ export class ConnectorComponent implements OnInit {
       case 'familysearch':
         this.connectText = 'Sign In with FamilySearch';
         break;
-      case 'facebook':
-        this.connectText = 'Sorry, our Facebook connection is suspended';
-        break;
     }
   }
 
@@ -192,7 +189,7 @@ export class ConnectorComponent implements OnInit {
     switch (this.connector.type) {
       case 'type.connector.facebook':
         template = `
-        We have suspended our Facebook integration.
+        We have suspended our Facebook integration indefinitely.
         <br><br>
         Click <a href='https://www.permanent.org/blog/'>here</a> to learn more.
         <br>
