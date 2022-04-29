d8b1c6f599258555a9d94ba840e9cd66e264a7ee
Jason Owen <jasonaowen@opentechstrategies.com>
2022-04-29 15:44:30 -0700

    Remove unused <pr-search> component
    The <pr-search> component is unused, as evidenced by its title ("Search
    Test") and lack of styling. The functionality it offers is duplicated
    in the <pr-public> component, which is correctly styled.
    
    Delete the <pr-search> component.

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -11,7 +11,6 @@ import { CoreModule } from '@core/core.module';
 import { FileBrowserModule } from '@fileBrowser/file-browser.module';
 import { PublicComponent } from './components/public/public.component';
 import { ItemNotFoundComponent } from './components/item-not-found/item-not-found.component';
-import { SearchComponent } from './components/search/search.component';
 import { PublicArchiveComponent } from './components/public-archive/public-archive.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
 import { PublicProfileComponent } from './components/public-profile/public-profile.component';
@@ -22,7 +21,6 @@ import { DialogModule } from '../dialog/dialog.module';
   declarations: [
     PublicComponent,
     ItemNotFoundComponent,
-    SearchComponent,
     PublicArchiveComponent,
     SearchBoxComponent,
     PublicProfileComponent,
--- src/app/public/public.routes.ts
+++ src/app/public/public.routes.ts
@@ -5,7 +5,6 @@ import { PublicComponent } from './components/public/public.component';
 import { PublishResolveService } from './resolves/publish-resolve.service';
 import { PublishArchiveResolveService } from './resolves/publish-archive-resolve.service';
 import { ItemNotFoundComponent } from './components/item-not-found/item-not-found.component';
-import { SearchComponent } from './components/search/search.component';
 import { PublicArchiveComponent } from './components/public-archive/public-archive.component';
 import { PublicArchiveResolveService } from './resolves/public-archive-resolve.service';
 import { PublicRootResolveService } from './resolves/public-root-resolve.service';
@@ -35,10 +34,6 @@ export const routes: RoutesWithData = [
       {
         path: 'error',
         component: ItemNotFoundComponent
-      },
-      {
-        path: 'search',
-        component: SearchComponent
       },
       {
         path: 'archive/:publicArchiveNbr',
