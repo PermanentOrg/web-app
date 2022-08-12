dec48ea11559d10715a126eec1f2dc3ff4365600
Natalie Martin <natalie@permanent.org>
2022-08-12 09:58:40 -0700

    List initial Featured Archives
    Include the actual production data for the featured archives, from the
    Permanent homepage. Hopefully all this stuff gets stored in a database
    at some point!
    
    PER-8683: Public Galler Landing Page (static)

--- src/app/gallery/data/featured.ts
+++ src/app/gallery/data/featured.ts
@@ -1,3 +1,55 @@
 import { FeaturedArchive } from '../types/featured-archive';
 
-export const featuredArchives: FeaturedArchive[] = [];
+// The featured archives are stored in an array of FeaturedArchive objects.
+// To use in .env, simply make an array of these objects in JSON format.
+
+export const featuredArchives: FeaturedArchive[] = [
+  {
+    archiveNbr: '03pw-0000',
+    name: 'The Astoria Public Archive',
+    type: 'type.archive.organization',
+    description: '',
+  },
+  {
+    archiveNbr: '03z2-0000',
+    name: 'The Paxman - Moody Archive',
+    type: 'type.archive.organization',
+    description: '',
+  },
+  {
+    archiveNbr: 'profile',
+    name: 'The Maine Genealogy Archive',
+    type: 'type.archive.family',
+    description: '',
+  },
+  {
+    archiveNbr: '03fm-0000',
+    name: 'The Pinson, Tarrant, Tuagilelagi Archive',
+    type: 'type.archive.family',
+    description: '',
+  },
+  {
+    archiveNbr: '02zq-0000',
+    name: 'The Jewish Historical Society of the Upper Midwest Archive',
+    type: 'type.archive.organization',
+    description: '',
+  },
+  {
+    archiveNbr: '00h3-0000',
+    name: 'The Tia Stenson Family History Archive',
+    type: 'type.archive.family',
+    description: '',
+  },
+  {
+    archiveNbr: '0532-0000',
+    name: 'The Soap Box Derby Archive',
+    type: 'type.archive.family',
+    description: '',
+  },
+  {
+    archiveNbr: '03fw-0000',
+    name: 'The Cape Cod Makers Archive',
+    type: 'type.archive.family',
+    description: '',
+  },
+];
