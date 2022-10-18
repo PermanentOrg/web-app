e6f9efc885f8b1bbbe3fb6061fe31cc23e49b5af
Liam Lloyd <wlloyd@g.hmc.edu>
2022-10-18 12:01:28 -0700

    Update dates on downtime notice banner

--- src/app/announcement/data/events.ts
+++ src/app/announcement/data/events.ts
@@ -2,9 +2,9 @@ import { AnnouncementEvent } from '../models/announcement-event';
 
 export const ANNOUNCEMENT_EVENTS: AnnouncementEvent[] = [
   {
-    start: new Date('2022-09-01T20:00:00-05:00').getTime(),
-    end: new Date('2022-09-08T20:10:00-05:00').getTime(),
+    start: new Date('2022-10-18T20:00:00-05:00').getTime(),
+    end: new Date('2022-10-25T20:10:00-05:00').getTime(),
     message:
-      'We will be performing scheduled maintenance on <time datetime="2022-09-08 18:00:00 PDT"><strong>Thursday, September 8th</strong> between <strong> 9pm - 12am ET / 6pm - 9pm PT.</strong></time> During this time, members will be unable to log into Permanent.org.',
+      'We will be performing scheduled maintenance on <time datetime="2022-10-25 18:00:00 PDT"><strong>Tuesday, October 25th</strong> between <strong> 9pm - 12am ET / 6pm - 9pm PT.</strong></time> During this time, members will be unable to log into Permanent.org.',
   },
 ];
