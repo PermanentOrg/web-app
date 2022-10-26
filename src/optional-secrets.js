8fb343bf121ed50eadf84f8969bd802a4b163ab4
Natalie Martin <natalie@permanent.org>
2022-10-26 11:14:03 -0700

    Add reCAPTCHA Site Key as optional secret
    The reCAPTCHA component requires an API key to use. Rather than forcing
    the application to break when an API key isn't specified (which we
    really don't need in testing environments), let's define the reCAPTCHA
    key as optional, with the idea that if no reCAPTCHA key is specified,
    the application just won't show the reCAPTCHA component.
    
    PER-9034: Add reCAPTCHA to sign in page

--- src/optional-secrets.js
+++ src/optional-secrets.js
@@ -3,6 +3,10 @@ const optionalSecrets = [
     name: 'FEATURED_ARCHIVES',
     default: '[]',
   },
+  {
+    name: 'RECAPTCHA_API_KEY',
+    default: '',
+  }
 ];
 
 module.exports = optionalSecrets;
