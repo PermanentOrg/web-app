7b42b343e14f3e1cd439be4d76469bae8510e4a8
Natalie Martin <natalie@permanent.org>
2023-03-13 14:06:58 -0500

    Add Legacy Contact API Stub
    The Legacy Contact API is still under construction, but the directive
    display UI needs to check for the presence of a legacy contact on the
    current account. Stub out a method to fetch the Legacy Contact for an
    account and create the type definition for the response.
    
    The `LegacyContact` type is modeled after the Legacy Contact table
    schema from the back-end.
    
    PER-9165: Display "No Plan" warning on directive display

--- src/app/shared/services/api/directive.repo.ts
+++ src/app/shared/services/api/directive.repo.ts
@@ -1,9 +1,14 @@
-import { ArchiveVO, Directive } from '@models/index';
+import { AccountVO, ArchiveVO, Directive, LegacyContact } from '@models/index';
 import { BaseRepo } from './base';
 
 export class DirectiveRepo extends BaseRepo {
   public async get(archive: ArchiveVO): Promise<Directive> {
     console.warn('Directive API is currently unimplemented.');
     return {} as Directive;
+  }
+
+  public async getLegacyContact(account: AccountVO): Promise<LegacyContact> {
+    console.warn('Directive API is currently unimplemented.');
+    return {} as LegacyContact;
   }
 }
