66fa19d0871ebef24120e660ec165a7abdb09fc0
Natalie Martin <natalie@permanent.org>
2023-04-24 20:33:59 -0700

    Uninstall and Reinstall Storybook
    Storybook had weird issues when upgrading Angular, so in order to fix
    the configuration and get everything working it's easiest to just
    uninstall and reinstall Storybook.
    
    PER-9245: Upgrade to Angular 15

---
+++ .storybook/preview.ts
@@ -0,0 +1,33 @@
+import type { Preview } from '@storybook/angular';
+
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const customViewports = {
+  archiveSettingsDesktop: {
+    name: 'archiveSettingsDesktop',
+    styles: {
+      width: '787px',
+      height: '700px',
+    },
+  },
+};
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    viewport: {
+      viewports: {
+        ...MINIMAL_VIEWPORTS,
+        ...customViewports,
+      },
+    },
+  },
+};
+
+export default preview;
