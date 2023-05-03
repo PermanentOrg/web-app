a2171b421ded09af8364a75f595ca8fe5f26e3cc
Natalie Martin <natalie@permanent.org>
2023-05-03 16:02:09 -0700

    Create New HttpV2Service
    The HttpService is used to handle the request/response structure of
    Permanent's V1 API. As we have begun to implement V2 versions of
    endpoints, we initially extended the HttpService with new methods to
    call V2 requests. However, we've now begun needing to call endpoints
    with more than just the POST method that the V1 Permanent API uses for
    each endpoint.
    
    To fix this problem in the most usable way, create a new service that
    more clearly separates the code that calls V1 and V2 endpoints. This new
    service now has four methods for HTTP verbs: GET, POST, PUT, and DELETE.
    It also uses the `responseClass` parameter in a bit more of a type safe
    way.
    
    In addition, there is no equivalent to the `sendRequestPromise` method
    of the V1 HttpService, as the caller can just call `.toPromise()` on
    their own.
    
    PER-9154: Implement Directives API

---
+++ src/app/shared/services/http-v2/http-v2.service.ts
@@ -0,0 +1,145 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { StorageService } from '../storage/storage.service';
+import { environment } from '@root/environments/environment';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Location } from '@angular/common';
+
+const CSRF_KEY = 'CSRF';
+
+type HttpMethod = 'post' | 'get' | 'put' | 'delete';
+type ResponseClass<T> = new (data: any) => T;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpV2Service {
+  protected static readonly HttpHeaders: HttpHeaders = new HttpHeaders({
+    'Request-Version': '2',
+    'Content-Type': 'application/json',
+  });
+  protected apiUrl = environment.apiUrl;
+
+  constructor(protected http: HttpClient, protected storage: StorageService) {}
+
+  public post<T>(
+    endpoint: string,
+    data: any = {},
+    responseClass?: ResponseClass<T>
+  ): Observable<T> {
+    return this.makeHttpClientRequest(endpoint, data, 'post', responseClass);
+  }
+
+  public get<T>(
+    endpoint: string,
+    data: any = {},
+    responseClass?: ResponseClass<T>
+  ): Observable<T> {
+    return this.makeHttpClientRequest(
+      this.getEndpointWithData(endpoint, data),
+      {},
+      'get',
+      responseClass
+    );
+  }
+
+  public put<T>(
+    endpoint: string,
+    data: any = {},
+    responseClass?: ResponseClass<T>
+  ): Observable<T> {
+    return this.makeHttpClientRequest(endpoint, data, 'put', responseClass);
+  }
+
+  public delete<T>(
+    endpoint: string,
+    data: any = {},
+    responseClass?: ResponseClass<T>
+  ): Observable<T> {
+    return this.makeHttpClientRequest(
+      this.getEndpointWithData(endpoint, data),
+      {},
+      'delete',
+      responseClass
+    );
+  }
+
+  protected getEndpointWithData(endpoint: string, data: any = {}): string {
+    const params = new HttpParams().appendAll(data);
+    return `${endpoint}?${params.toString()}`;
+  }
+
+  protected appendCsrf(data: any): Object {
+    return {
+      ...data,
+      csrf: this.storage.session.get(CSRF_KEY),
+    };
+  }
+
+  protected getFullUrl(endpoint: string): string {
+    return Location.joinWithSlash(this.apiUrl, endpoint);
+  }
+
+  protected getHeaders() {
+    return {
+      headers: HttpV2Service.HttpHeaders,
+    };
+  }
+
+  protected getObservableWithBody(
+    url: string,
+    data: any = {},
+    method: HttpMethod
+  ): Observable<unknown> {
+    if (method === 'put') {
+      return this.http.put(url, data, this.getHeaders());
+    }
+    return this.http.post(url, data, this.getHeaders());
+  }
+
+  protected getObservableWithNoBody(
+    url: string,
+    method: HttpMethod
+  ): Observable<unknown> {
+    if (method === 'delete') {
+      return this.http.delete(url, this.getHeaders());
+    }
+    return this.http.get(url, this.getHeaders());
+  }
+
+  protected getObservable(
+    endpoint: string,
+    data: any = {},
+    method: HttpMethod = 'post'
+  ): Observable<unknown> {
+    if (method === 'post' || method === 'put') {
+      return this.getObservableWithBody(
+        this.getFullUrl(endpoint),
+        this.appendCsrf(data),
+        method
+      );
+    }
+    return this.getObservableWithNoBody(this.getFullUrl(endpoint), method);
+  }
+
+  protected makeHttpClientRequest<T>(
+    endpoint: string,
+    data: any = {},
+    method: HttpMethod = 'post',
+    responseClass?: new (data: any) => T
+  ): Observable<T> {
+    const observable = this.getObservable(endpoint, data, method);
+    return observable.pipe(
+      map((response: Object) => {
+        if (response.hasOwnProperty('csrf')) {
+          this.storage.session.set(CSRF_KEY, response['csrf']);
+        }
+        if (responseClass) {
+          return new responseClass(response);
+        }
+        return response as T;
+      })
+    );
+  }
+}
