3fa2971b235168e2b34c405f06c28c3fac85324f
Natalie Martin <natalie@permanent.org>
2023-05-04 15:32:27 -0700

    Implement Directives API Methods
    This implements the Directive create/update/read methods in the API
    Service Directive repo with unit tests.
    
    Stela expects `archiveId` to be a string instead of a number, so the
    model for the DirectiveCreateRequest has been updated.
    
    PER-9154: Implement Directives API

--- src/app/shared/services/api/directive.repo.ts
+++ src/app/shared/services/api/directive.repo.ts
@@ -1,30 +1,46 @@
 import {
   AccountVO,
   ArchiveVO,
-  Directive,
+  DirectiveData,
   DirectiveCreateRequest,
   LegacyContact,
+  Directive,
 } from '@models/index';
 import { BaseRepo } from './base';
+import { getFirst } from '../http-v2/http-v2.service';
 
 export class DirectiveRepo extends BaseRepo {
   public async get(archive: ArchiveVO): Promise<Directive> {
-    console.warn('Directive API is currently unimplemented.');
-    return {} as Directive;
+    return getFirst(
+      this.httpV2.get(
+        `/v2/directive/archive/${archive.archiveId}`,
+        {},
+        Directive
+      )
+    ).toPromise();
   }
 
   public async getLegacyContact(account: AccountVO): Promise<LegacyContact> {
-    console.warn('Directive API is currently unimplemented.');
+    console.warn('Legacy Contact API is currently unimplemented.');
     return {} as LegacyContact;
   }
 
   public async create(directive: DirectiveCreateRequest): Promise<Directive> {
-    console.warn('Directive API is currently unimplemented.');
-    return {} as Directive;
+    return getFirst(
+      this.httpV2.post('/v2/directive', directive, Directive)
+    ).toPromise();
   }
 
-  public async update(directive: Partial<Directive>): Promise<Directive> {
-    console.warn('Directive API is currently unimplemented.');
-    return {} as Directive;
+  public async update(directive: Partial<DirectiveData>): Promise<Directive> {
+    if (!directive.directiveId) {
+      throw new Error(
+        'directiveID is required to update an existing Directive.'
+      );
+    }
+    const data = Object.assign({}, directive);
+    delete data.directiveId;
+    return getFirst(
+      this.httpV2.put(`/v2/directive/${directive.directiveId}`, data, Directive)
+    ).toPromise();
   }
 }
