88090c1eb7347817c6537417118d3047dfa9677c
Andrei Crisnic <crisnicandrei@gmail.com>
2023-05-04 12:23:35 +0300

    Run prettier on sidebar component

--- src/app/file-browser/components/sidebar/sidebar.component.ts
+++ src/app/file-browser/components/sidebar/sidebar.component.ts
@@ -1,7 +1,10 @@
 import { RecordVO } from '@root/app/models';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '@shared/services/data/data.service';
-import { HasSubscriptions, unsubscribeAll } from '@shared/utilities/hasSubscriptions';
+import {
+  HasSubscriptions,
+  unsubscribeAll,
+} from '@shared/utilities/hasSubscriptions';
 import { Subscription } from 'rxjs';
 import { some } from 'lodash';
 import { ItemVO, FolderVO, ArchiveVO, AccessRole } from '@models';
@@ -9,14 +12,13 @@ import { DataStatus } from '@models/data-status.enum';
 import { EditService } from '@core/services/edit/edit.service';
 import { AccountService } from '@shared/services/account/account.service';
 
-
 import type { KeysOfType } from '@shared/utilities/keysoftype';
 
-type SidebarTab =  'info' | 'details' | 'sharing' | 'views';
+type SidebarTab = 'info' | 'details' | 'sharing' | 'views';
 @Component({
   selector: 'pr-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit, OnDestroy, HasSubscriptions {
   currentTab: SidebarTab = 'info';
@@ -35,20 +37,18 @@ export class SidebarComponent implements OnInit, OnDestroy, HasSubscriptions {
   canShare: boolean;
   canUseViews: boolean;
 
-  originalFileExtension:string = ''
-  permanentFileExtension:string = ''
-
-  
+  originalFileExtension: string = '';
+  permanentFileExtension: string = '';
 
   constructor(
     private dataService: DataService,
     private editService: EditService,
-    private accountService: AccountService,
+    private accountService: AccountService
   ) {
     this.currentArchive = this.accountService.getArchive();
 
     this.subscriptions.push(
-      this.dataService.selectedItems$().subscribe(async selectedItems => {
+      this.dataService.selectedItems$().subscribe(async (selectedItems) => {
         if (!selectedItems.size) {
           this.selectedItem = this.dataService.currentFolder;
           this.selectedItems = null;
@@ -61,11 +61,17 @@ export class SidebarComponent implements OnInit, OnDestroy, HasSubscriptions {
         }
 
         this.isRootFolder = this.selectedItem?.type?.includes('root');
-        this.isPublicItem = this.selectedItem?.type?.includes('public') || this.selectedItem?.folder_linkType?.includes('public');
+        this.isPublicItem =
+          this.selectedItem?.type?.includes('public') ||
+          this.selectedItem?.folder_linkType?.includes('public');
 
         this.checkPermissions();
 
-        this.canUseViews = !this.isRootFolder && this.isPublicItem && this.selectedItem && this.selectedItem.isFolder;
+        this.canUseViews =
+          !this.isRootFolder &&
+          this.isPublicItem &&
+          this.selectedItem &&
+          this.selectedItem.isFolder;
 
         if (this.isRootFolder) {
           this.setCurrentTab('info');
@@ -77,41 +83,38 @@ export class SidebarComponent implements OnInit, OnDestroy, HasSubscriptions {
 
         if (this.selectedItem !== this.dataService.currentFolder) {
           const items = this.selectedItems || [this.selectedItem];
-          this.isLoading = some(items, i => i.dataStatus < DataStatus.Full);
+          this.isLoading = some(items, (i) => i.dataStatus < DataStatus.Full);
           if (this.isLoading) {
             await this.dataService.fetchFullItems(items);
             this.isLoading = false;
           }
         }
 
-        
-        if(this.selectedItem instanceof RecordVO){
-          const originalContentType = this.selectedItem.FileVOs[0].type.split('.').pop()
-          let convertedContentType = ''
+        if (this.selectedItem instanceof RecordVO) {
+          const originalContentType = this.selectedItem.FileVOs[0].type
+            .split('.')
+            .pop();
+          let convertedContentType = '';
 
-          if(this.selectedItem.FileVOs[1]){
-            convertedContentType = this.selectedItem.FileVOs[1].type.split('.').pop()
+          if (this.selectedItem.FileVOs[1]) {
+            convertedContentType = this.selectedItem.FileVOs[1].type
+              .split('.')
+              .pop();
+          } else {
+            convertedContentType = originalContentType;
           }
-          else{
-            convertedContentType = originalContentType
-          }
-       
 
-        this.originalFileExtension = originalContentType
-        this.permanentFileExtension = convertedContentType
-        }else{
-          this.originalFileExtension = ''
-          this.permanentFileExtension = ''
+          this.originalFileExtension = originalContentType;
+          this.permanentFileExtension = convertedContentType;
+        } else {
+          this.originalFileExtension = '';
+          this.permanentFileExtension = '';
         }
-        
       })
     );
   }
 
-  ngOnInit() {
-    
-   
-  }
+  ngOnInit() {}
 
   ngOnDestroy() {
     unsubscribeAll(this.subscriptions);
@@ -119,16 +122,27 @@ export class SidebarComponent implements OnInit, OnDestroy, HasSubscriptions {
 
   checkPermissions() {
     const items = this.selectedItems || [this.selectedItem];
-    const viewOnly = some(items, i => i.accessRole === 'access.role.viewer' || i.accessRole === 'access.role.contributor');
+    const viewOnly = some(
+      items,
+      (i) =>
+        i.accessRole === 'access.role.viewer' ||
+        i.accessRole === 'access.role.contributor'
+    );
 
-    this.canEdit = !viewOnly && this.accountService.checkMinimumArchiveAccess(AccessRole.Editor);
+    this.canEdit =
+      !viewOnly &&
+      this.accountService.checkMinimumArchiveAccess(AccessRole.Editor);
 
     if (items.length !== 1) {
       this.canShare = false;
     } else {
-      this.canShare = !this.isPublicItem &&
+      this.canShare =
+        !this.isPublicItem &&
         !this.isRootFolder &&
-        this.accountService.checkMinimumAccess(this.selectedItem.accessRole, AccessRole.Owner);
+        this.accountService.checkMinimumAccess(
+          this.selectedItem.accessRole,
+          AccessRole.Owner
+        );
     }
   }
 
@@ -159,7 +173,10 @@ export class SidebarComponent implements OnInit, OnDestroy, HasSubscriptions {
   }
 
   getFolderContentsCount() {
-    if (this.selectedItem instanceof FolderVO && this.selectedItem.FolderSizeVO) {
+    if (
+      this.selectedItem instanceof FolderVO &&
+      this.selectedItem.FolderSizeVO
+    ) {
       const fileCount = this.selectedItem.FolderSizeVO.allRecordCountShallow;
       const folderCount = this.selectedItem.FolderSizeVO.allFolderCountShallow;
       const fileLabel = fileCount > 1 ? 'files' : 'file';
@@ -176,8 +193,4 @@ export class SidebarComponent implements OnInit, OnDestroy, HasSubscriptions {
       }
     }
   }
-
-
-
-
 }
