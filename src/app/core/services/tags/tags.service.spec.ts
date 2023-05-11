a5dd9a6bbf96ef0750a145bf2ce4254d0cc023dc
Natalie Martin <natalie@permanent.org>
2023-05-11 11:23:04 -0700

    Remove shallow-render from TagsService tests
    Somehow, tests from the TagsService started to fail. The reason is that
    they were using shallow-render which was improperly mocking the
    ApiService and not injecting in the HttpClientTestingModule.
    
    This fixes that error. There have been a lot of shallow-render related
    issues with unit testing lately and it begs the question: should we
    continue to use it? I still think it's valuable for making Angular unit
    tests easier to read and write, but it's concerning how often they can
    randomly break.

--- src/app/core/services/tags/tags.service.spec.ts
+++ src/app/core/services/tags/tags.service.spec.ts
@@ -1,40 +1,49 @@
 /* @format */
 import { TestBed } from '@angular/core/testing';
-import { Shallow } from 'shallow-render';
 import { Subscription } from 'rxjs';
 
 import { TagsService } from './tags.service';
 import { AccountService } from '@shared/services/account/account.service';
-import { AppModule } from '../../../app.module';
 import { ArchiveVO, RecordVO } from '@models';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ApiService } from '@shared/services/api/api.service';
 
 describe('TagsService', () => {
-  let shallow: Shallow<TagsService>;
+  let service: TagsService;
 
   beforeEach(() => {
-    shallow = new Shallow(TagsService, AppModule).mock(AccountService, {
-      getArchive: () => new ArchiveVO({ archiveId: 1 }),
-      archiveChange: {
-        subscribe: (callback) => new Subscription(),
-      },
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        ApiService,
+        {
+          provide: AccountService,
+          useValue: {
+            getArchive: () => new ArchiveVO({ archiveId: 1 }),
+            archiveChange: {
+              subscribe: (callback) => new Subscription(),
+            },
+          },
+        },
+      ],
     });
+    TestBed.inject(ApiService);
+    service = TestBed.inject(TagsService);
   });
 
   it('should be created', () => {
-    const { instance } = shallow.createService();
-    expect(instance).toBeTruthy();
+    expect(service).toBeTruthy();
   });
 
   it('should only cache tags from the current archive', () => {
-    const { instance } = shallow.createService();
     const item = new RecordVO({
       TagVOs: [
         { tagId: 1, name: 'testOne', archiveId: 1 },
         { tagId: 2, name: 'testTwo', archiveId: 2 },
       ],
     });
-    instance.checkTagsOnItem(item);
+    service.checkTagsOnItem(item);
 
-    expect(instance.getTags().length).toEqual(1);
+    expect(service.getTags().length).toEqual(1);
   });
 });
