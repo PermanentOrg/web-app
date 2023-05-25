ad47dd1c8c3271bd668fe9f109f11f1ce7d1b5c9
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2023-05-25 13:46:55 +0300

    PER-9268-wrote a function that checks if an url is a facebook url

--- src/app/public/components/public-archive-web-links/public-archive-web-links.component.ts
+++ src/app/public/components/public-archive-web-links/public-archive-web-links.component.ts
@@ -12,6 +12,7 @@ export class PublicArchiveWebLinksComponent implements OnInit {
   @Input() emails: string[] = [];
   @Input() websites: string[] = [];
 
+
   constructor() {}
 
   ngOnInit(): void {}
@@ -25,5 +26,14 @@ export class PublicArchiveWebLinksComponent implements OnInit {
 
   navigateToMail(mail:string): void {
     window.open('mailto:' + mail, '_blank');
+  }
+
+  isFacebookUrl(url) {
+    try {
+      const parsedUrl = new URL(url.startsWith('http') ? url : 'https://' + url);
+      return ['www.facebook.com', 'm.facebook.com', 'facebook.com', 'fb.me'].includes(parsedUrl.hostname);
+    } catch (_) {
+      return false; // invalid URL
+    }
   }
 }
