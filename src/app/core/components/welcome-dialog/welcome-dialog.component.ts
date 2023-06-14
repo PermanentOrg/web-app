59ff07a05f85a97b87a0ffee4640357d0268bdb6
Natalie Martin <natalie@permanent.org>
2023-06-14 08:01:46 -0500

    Show different welcome message for default signups
    Show a welcome message reminding users who signed up with no special
    invite code to check their email to redeem their storage.
    
    Note that this solution does have an edge case bug: If a user signs up
    with a specific promo code but does not complete onboarding and does so
    later on another device/browser, they will be shown this new message
    instead of the default message, which may be inaccurate. We've accepted
    this as a good enough solution for the time being since there's no way
    to get a user's invite code via the API right now.
    
    PER-9280: Add message to redeem storage for earlyb1rd signups

--- src/app/core/components/welcome-dialog/welcome-dialog.component.ts
+++ src/app/core/components/welcome-dialog/welcome-dialog.component.ts
@@ -1,27 +1,31 @@
+/* @format */
 import { Component, OnInit } from '@angular/core';
-import { DialogRef, DIALOG_DATA } from '@root/app/dialog/dialog.module';
+import { DialogRef } from '@root/app/dialog/dialog.module';
 import { AccountService } from '@shared/services/account/account.service';
 import { PrConstantsService } from '@shared/services/pr-constants/pr-constants.service';
 
 @Component({
   selector: 'pr-welcome-dialog',
   templateUrl: './welcome-dialog.component.html',
-  styleUrls: ['./welcome-dialog.component.scss']
+  styleUrls: ['./welcome-dialog.component.scss'],
 })
 export class WelcomeDialogComponent implements OnInit {
   public archiveName: string;
   public accessRole: string;
+  public isEarlyBird: boolean;
 
   constructor(
     private dialogRef: DialogRef,
     private account: AccountService,
     private constants: PrConstantsService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     const archive = this.account.getArchive();
     this.archiveName = archive.fullName;
     this.accessRole = this.constants.translate(archive.accessRole);
+    this.isEarlyBird =
+      !this.account.inviteCode || this.account.inviteCode === 'earlyb1rd';
   }
 
   public close(): void {
