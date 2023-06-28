8249d7ad352a79674ca4b24af6fd506ad8c8221d
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2023-06-28 14:53:30 +0300

    PER-9242
    Wrote tests for the new component

--- src/app/core/components/archive-payer/archive-payer.component.spec.ts
+++ src/app/core/components/archive-payer/archive-payer.component.spec.ts
@@ -4,15 +4,30 @@ import { AccountService } from '@shared/services/account/account.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ArchivePayerComponent } from './archive-payer.component';
+import { ArchiveVO, AccountVO } from '@models/index';
 
 describe('ArchivePayerComponent', () => {
   let component: ArchivePayerComponent;
   let fixture: ComponentFixture<ArchivePayerComponent>;
+  let mockAccountService;
+
+  beforeEach(() => {
+    mockAccountService = {
+      getAccount: jasmine
+        .createSpy('getAccount')
+        .and.returnValue({ accountId: '1' }),
+    };
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ArchivePayerComponent],
-      providers: [HttpClient, HttpHandler, AccountService, MessageService],
+      providers: [
+        { provide: AccountService, useValue: mockAccountService },
+        HttpClient,
+        HttpHandler,
+        MessageService,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ArchivePayerComponent);
@@ -22,5 +37,127 @@ describe('ArchivePayerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('displays no payer when there is no payer id assigned to the account', () => {
+    component.hasPayer = false;
+    fixture.detectChanges();
+    const divElement = fixture.nativeElement.querySelector('#no-payer');
+    expect(divElement).toBeTruthy();
+  });
+
+  it('displays payer data when there is a payer id assigned to the account', () => {
+    const payer = new AccountVO({});
+    payer.fullName = 'Test name';
+    payer.primaryEmail = 'test@email.com';
+    payer.accessRole = 'access.role.owner';
+
+    component.payer = payer;
+    component.hasPayer = true;
+
+    fixture.detectChanges();
+    const divElement = fixture.nativeElement.querySelector('#with-payer');
+    expect(divElement).toBeTruthy();
+  });
+
+  it('displays the current payer data if the logged account is different than the payer', () => {
+    const payer = new AccountVO({});
+
+    payer.accountId = '1';
+    payer.fullName = 'Test name';
+    payer.primaryEmail = 'test@email.com';
+    payer.accessRole = 'access.role.owner';
+
+    component.hasPayer = true;
+    component.isPayerDifferentThanLoggedUser = true;
+    component.payer = payer;
+
+    fixture.detectChanges();
+    const div = fixture.nativeElement.querySelector('#is-not-payer');
+    expect(div).toBeTruthy();
+  });
+
+  it('displays the current payer data if the logged account is different than the payer', () => {
+    component.hasPayer = true;
+    component.isPayerDifferentThanLoggedUser = false;
+
+    const payer = new AccountVO({});
+    payer.fullName = 'Test name';
+    payer.primaryEmail = 'test@email.com';
+    payer.accessRole = 'access.role.owner';
+
+    fixture.detectChanges();
+    const div = fixture.nativeElement.querySelector('#is-payer');
+    expect(div).toBeTruthy();
+  });
+
+  it('should set hasPayer to true when archive.payerAccountId exists', () => {
+    const archive = new ArchiveVO({});
+    archive.payerAccountId = '1';
+
+    const payer = new AccountVO({});
+    payer.fullName = 'Test name';
+    payer.primaryEmail = 'test@email.com';
+    payer.accessRole = 'access.role.owner';
+
+    component.archive = archive;
+    component.payer = payer;
+    component.ngOnInit();
+
+    fixture.detectChanges();
+    expect(component.hasPayer).toEqual(true);
+  });
+
+  it('should set hasPayer to false when archive.payerAccountId does not exist', () => {
+    const archive = new ArchiveVO({});
+    archive.payerAccountId = null;
+
+    const payer = new AccountVO({});
+    payer.fullName = 'Test name';
+    payer.primaryEmail = 'test@email.com';
+    payer.accessRole = 'access.role.owner';
+
+    component.archive = archive;
+    component.payer = payer;
+    
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.hasPayer).toEqual(false);
+  });
+
+  it('should check if the payer is the same as the account id', () => {
+    const archive = new ArchiveVO({});
+    archive.payerAccountId = '1';
+    component.archive = archive;
+
+    const payer = new AccountVO({});
+    payer.fullName = 'Test name';
+    payer.primaryEmail = 'test@email.com';
+    payer.accessRole = 'access.role.owner';
+
+    component.payer = payer;
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(component.isPayerDifferentThanLoggedUser).toEqual(false);
+  });
+
+  it('should check if the payer is not the same as the account id', () => {
+    const archive = new ArchiveVO({});
+    archive.payerAccountId = '2';
+    component.archive = archive;
+
+    const payer = new AccountVO({});
+    payer.fullName = 'Test name';
+    payer.primaryEmail = 'test@email.com';
+    payer.accessRole = 'access.role.owner';
+
+    component.payer = payer;
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(component.isPayerDifferentThanLoggedUser).toEqual(true);
   });
 });
