b6bd1915b7b1fb01f901e99d90cb5360a7c5f2a0
Natalie Martin <natalie@permanent.org>
2023-07-11 10:26:02 -0700

    Adjust multipart uploads
    Retry uploads up to 10 times (an arbitrary choice), change the multipart
    upload threshold to be 100MB, and adjust some variables.
    
    PER-9197: Use Multipart Upload URLs for large files

--- src/app/core/services/upload/uploader.ts
+++ src/app/core/services/upload/uploader.ts
@@ -21,6 +21,8 @@ const buildForm = (fields: object, file: File) => {
 
 @Injectable()
 export class Uploader {
+  protected readonly tenMB = 10 * 1024 * 1024;
+
   constructor(private api: ApiService, private httpClient: HttpClient) {}
 
   private getUploadData = async (item: UploadItem) => {
@@ -63,32 +65,52 @@ export class Uploader {
     return this.registerRecord(item, destinationUrl);
   };
 
+  private uploadToMultipartUrl = async (
+    url: string,
+    item: UploadItem,
+    filePointer: number,
+    eTags: string[]
+  ) => {
+    const response = await this.httpClient
+      .put(url, item.file.slice(filePointer, filePointer + this.tenMB), {
+        headers: {
+          'Content-Type': item.file.type || 'application/octet-stream',
+        },
+        observe: 'response',
+        reportProgress: true,
+        responseType: 'json',
+        withCredentials: false,
+      })
+      .toPromise();
+    const etag = response.headers.get('etag').replace(/^"(.+?)"$/, '$1');
+    const index = Math.floor(filePointer / this.tenMB);
+    eTags[index] = etag;
+  };
+
   private uploadMultipart = async (
     item: UploadItem,
     emitProgress: (n: number) => void
   ) => {
-    const tenMB = 10 * 1024 * 1024;
-    const { urls, uploadId, key } =
-      await this.api.record.getMultipartUploadURLs(item.file.size);
-    let filePointer = 0;
     const size = item.file.size;
-    const eTags = [];
+    const { urls, uploadId, key } =
+      await this.api.record.getMultipartUploadURLs(size);
+    let filePointer = 0;
+    const eTags: string[] = [];
     for (const url of urls) {
-      const response = await this.httpClient
-        .put(url, item.file.slice(filePointer, filePointer + tenMB), {
-          headers: {
-            'Content-Type': item.file.type || 'application/octet-stream',
-          },
-          observe: 'response',
-          reportProgress: true,
-          responseType: 'json',
-          withCredentials: false,
-        })
-        .toPromise();
-      const etag = response.headers.get('etag').replace(/^"(.+?)"$/, '$1');
-      const index = Math.floor(filePointer / tenMB);
-      eTags[index] = etag;
-      filePointer += tenMB;
+      let uploaded = false;
+      let retries = 0;
+      const retryLimit = 10;
+      while (!uploaded) {
+        try {
+          await this.uploadToMultipartUrl(url, item, filePointer, eTags);
+          uploaded = true;
+        } catch {
+          if (retries++ > retryLimit) {
+            throw new Error('Multipart upload retry limit exceeded');
+          }
+        }
+      }
+      filePointer += this.tenMB;
       const progress = Math.min(filePointer / size, 1);
       emitProgress(progress);
     }
@@ -105,7 +127,7 @@ export class Uploader {
     item: UploadItem,
     emitUploadProgress: (e: number) => void
   ): Promise<any> {
-    const multiPartSize = 5 * 1024 * 1024 * 1024; // Five GB
+    const multiPartSize = 100 * 1024 * 1024; // 100 MB
     emitUploadProgress(0);
 
     const httpProgress = (e: HttpEvent<any>) => {
