c047e476b570abce51fbc47aaeef0dbcd9992cdd
Natalie Martin <natalie@permanent.org>
2023-09-01 12:49:00 -0700

    Add tests for SearchService.parseSearchTerm
    SearchService previously had no tests. (Or to be more specific, it only
    had commented out test boilerplate!) Let's fix that and add some tests.
    First off, add tests that cover the SearchService.parseSearchTerm
    function. These are grouped into their own describe block since the
    function is a bit complex and the tests have their own helper assertion.
    
    Some of the functionality of this function is a bit unexpected
    (sometimes it returns `undefined` when really it should probably be
    returning an empty string), but I've written the test cases to match
    behavior for now. If we want to change this down the line, we'll have
    test cases to guard against breaking other parts of service!

--- src/app/search/services/search.service.spec.ts
+++ src/app/search/services/search.service.spec.ts
@@ -1,16 +1,106 @@
-// import { TestBed } from '@angular/core/testing';
+/* @format */
+import { TestBed } from '@angular/core/testing';
+import { Subject } from 'rxjs';
 
-// import { SearchService } from './search.service';
+import { ApiService } from '@shared/services/api/api.service';
+import { DataService } from '@shared/services/data/data.service';
+import { TagsService } from '@core/services/tags/tags.service';
+import { TagVO, TagVOData } from '@models/tag-vo';
+import { SearchService } from './search.service';
 
-// describe('SearchService', () => {
-//   let service: SearchService;
+class MockApiService {}
+class MockDataService {
+  public currentFolderChange = new Subject<void>();
+}
+class MockTagsService {
+  private tags: TagVOData[] = [];
+  private tagsSubject = new Subject<TagVOData[]>();
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(SearchService);
-//   });
+  public getTags$(): Subject<TagVOData[]> {
+    return this.tagsSubject;
+  }
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  public getTagByName(name: string): TagVOData {
+    return this.tags.find((t) => t.name === name);
+  }
+
+  public setTags(tags: TagVOData[]): void {
+    this.tags = tags;
+    this.tagsSubject.next(this.tags);
+  }
+}
+
+describe('SearchService', () => {
+  let service: SearchService;
+  let tags: MockTagsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [SearchService],
+    });
+    TestBed.overrideProvider(ApiService, { useValue: new MockApiService() });
+    TestBed.overrideProvider(DataService, { useValue: new MockDataService() });
+    TestBed.overrideProvider(TagsService, { useValue: new MockTagsService() });
+    service = TestBed.inject(SearchService);
+    tags = TestBed.inject(TagsService) as any as MockTagsService;
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('ParseSearchTerm', () => {
+    it('should generate a data structure for an empty search', () => {
+      expectSearchToBe(service.parseSearchTerm(''), '', []);
+    });
+
+    it('should generate data for a basic search', () => {
+      expectSearchToBe(service.parseSearchTerm('Potato'), 'Potato', []);
+    });
+
+    it('should remove tag tokens from search', () => {
+      expectSearchToBe(
+        service.parseSearchTerm('tag:"NonExistentTag"'),
+        undefined,
+        []
+      );
+    });
+
+    it('should load tag tokens into the tags array', () => {
+      tags.setTags([{ name: 'Potato' }]);
+      expectSearchToBe(service.parseSearchTerm('tag:"Potato"'), undefined, [
+        { name: 'Potato' },
+      ]);
+    });
+
+    it('should preserve the rest of the search', () => {
+      tags.setTags([{ name: 'Potato' }]);
+      expectSearchToBe(
+        service.parseSearchTerm('Hello tag:"Potato" World'),
+        'Hello World',
+        [{ name: 'Potato' }]
+      );
+      expectSearchToBe(
+        service.parseSearchTerm('tag:"Potato" Hello World tag:"Potato"'),
+        'Hello World',
+        [{ name: 'Potato' }, { name: 'Potato' }]
+      );
+    });
+
+    it('handles tag edge cases', () => {
+      tags.setTags([{ name: 'tag:Test' }]);
+      expectSearchToBe(service.parseSearchTerm('tag:"tag:"Test""'), undefined, [
+        { name: 'tag:Test' },
+      ]);
+    });
+
+    function expectSearchToBe(
+      search: [string, TagVOData[]],
+      expectedSearch: string,
+      expectedTags: TagVOData[]
+    ) {
+      expect(search[0]).toBe(expectedSearch);
+      expect(search[1]).toEqual(expectedTags);
+    }
+  });
+});
