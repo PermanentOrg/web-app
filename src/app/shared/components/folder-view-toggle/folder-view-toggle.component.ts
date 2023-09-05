9073b2bd5fac8424a36fda3068d8fbcaa5f503c2
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2023-09-05 14:21:43 +0300

    PER-9334
    Display the grid-list buttons all the time

--- src/app/shared/components/folder-view-toggle/folder-view-toggle.component.ts
+++ src/app/shared/components/folder-view-toggle/folder-view-toggle.component.ts
@@ -1,14 +1,15 @@
+/* @format */
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FolderView } from '@shared/services/folder-view/folder-view.enum';
 import { FolderViewService } from '@shared/services/folder-view/folder-view.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import debug from 'debug';
-import { debugSubscribable } from '@shared/utilities/debug';
-import { getLastChildRouteDataOperator } from '@shared/utilities/router';
 import { Subscription, Observable } from 'rxjs';
-import { unsubscribeAll, HasSubscriptions } from '@shared/utilities/hasSubscriptions';
-import { map } from 'rxjs/operators';
+import {
+  unsubscribeAll,
+  HasSubscriptions,
+} from '@shared/utilities/hasSubscriptions';
 
 interface FolderViewToggleOption {
   iconClass: 'reorder' | 'view_module';
@@ -19,55 +20,43 @@ interface FolderViewToggleOption {
 @Component({
   selector: 'pr-folder-view-toggle',
   templateUrl: './folder-view-toggle.component.html',
-  styleUrls: ['./folder-view-toggle.component.scss']
+  styleUrls: ['./folder-view-toggle.component.scss'],
 })
-export class FolderViewToggleComponent implements OnInit, OnDestroy, HasSubscriptions {
+export class FolderViewToggleComponent
+  implements OnInit, OnDestroy, HasSubscriptions
+{
   currentFolderView: FolderView;
   folderViews: FolderViewToggleOption[] = [
     {
       iconClass: 'reorder',
       folderView: FolderView.List,
-      tooltip: 'fileList.viewToggle.list'
+      tooltip: 'fileList.viewToggle.list',
     },
     {
       iconClass: 'view_module',
       folderView: FolderView.Grid,
-      tooltip: 'fileList.viewToggle.grid'
+      tooltip: 'fileList.viewToggle.grid',
     },
   ];
 
-  showToggle: boolean;
   showToggle$: Observable<boolean>;
 
   private debug = debug('component:folderViewToggle');
   subscriptions: Subscription[] = [];
   constructor(
     private folderView: FolderViewService,
-    private router: Router,
-    private route: ActivatedRoute
   ) {
     this.currentFolderView = this.folderView.folderView;
     this.debug('init view %o', this.currentFolderView);
 
     this.subscriptions.push(
-      folderView.viewChange.subscribe(view => {
+      folderView.viewChange.subscribe((view) => {
         this.currentFolderView = view;
       })
     );
-
-    this.subscriptions.push(
-      this.router.events.pipe(
-        getLastChildRouteDataOperator(),
-        map(routeData => !!routeData.showFolderViewToggle)
-      ).subscribe(showToggle => {
-        this.showToggle = showToggle;
-        this.debug('show %o', this.showToggle);
-      })
-    );
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy() {
     unsubscribeAll(this.subscriptions);
@@ -77,5 +66,4 @@ export class FolderViewToggleComponent implements OnInit, OnDestroy, HasSubscrip
     this.debug('set view %o', option.folderView);
     this.folderView.setFolderView(option.folderView);
   }
-
 }
