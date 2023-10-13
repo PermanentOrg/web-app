a10ee149d706e0f986faaa51959316bf24143ba1
Natalie Martin <natalie@permanent.org>
2023-10-13 09:55:34 -0500

    Update to karma-coverage package
    Update to the new karma-coverage package and configure Karma to use it
    instead of the old and deprecated karma-coverage-istanbul-reporter
    package. Generate a new karma config for our current version of Angular,
    and then copy some of our old configuration into it. Update angular.json
    to point to the new location of the Karma config file, which is now at
    the root of the project (where it probably should be).
    
    PER-9359: Add test coverage tool to web-app

---
+++ karma.conf.js
@@ -0,0 +1,52 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('karma-spec-reporter'),
+      require('karma-junit-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/mdot'),
+      subdir: '.',
+      reporters: [
+        { type: 'html', subdir: 'html' },
+        { type: 'lcov', subdir: 'lcov' },
+        { type: 'text-summary' },
+      ],
+    },
+    reporters: ['progress', 'coverage', 'kjhtml', 'junit', 'spec'],
+    browsers: ['ChromeHeadless'],
+    restartOnFileChange: true,
+    customLaunchers: {
+      chrome_headless_no_sandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-gpu'],
+      },
+      ChromeDebug: {
+        base: 'ChromeHeadless',
+        flags: ['--remote-debugging-port=9333'],
+      },
+    },
+  });
+};
