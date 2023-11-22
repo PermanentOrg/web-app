4bd8699f081aeb8f9100e9475b46de86769888a5
Natalie Martin <natalie@permanent.org>
2023-11-22 07:56:51 -0700

    Create RouteHistoryModule
    The web-app currently relies on a package called ngx-route-history for
    some functionality. Unfortunately, this package does not seem to be
    maintained anymore and it is incompatible with Angular 16. Since this
    was a custom package made by a previous developer, it makes more sense
    for the web-app to fully own this code so it's always compatible.
    
    Create the RouteHistoryModule with the RouteHistory service, and import
    the tests from the original package. The tests were modified to use real
    async instead of the `fakeAsync` that was used in older Angular tests,
    as well and the `ngOnDestroy` test was rewritten as well to be simpler.
    
    The service itself was not really adapted from the original, instead
    developed with Test Driven Development using the existing tests.
    Although the service is so simple that the implementations end up being
    very similar.
    
    PER-9399: Upgrade to Angular 16

---
+++ src/app/route-history/route-history.service.spec.ts
@@ -0,0 +1,66 @@
+/* @format */
+/*
+These tests were adapted from the tests of the ngx-route-history package:
+https://github.com/andrewatwood/ngx-route-history/blob/master/tests/services/route-history.service.spec.ts
+*/
+import { TestBed } from '@angular/core/testing';
+import { RouteHistoryService } from './route-history.service';
+import { Component } from '@angular/core';
+import { Router, Routes } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+@Component({
+  selector: 'pr-dummy',
+  template: 'test',
+})
+class DummyComponent {}
+
+const testRoutes: Routes = [
+  { path: '', component: DummyComponent },
+  { path: 'home', component: DummyComponent },
+  { path: 'profile', component: DummyComponent },
+];
+
+describe('RouteHistoryService', () => {
+  let service: RouteHistoryService;
+  let router: Router;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes(testRoutes)],
+    });
+    service = TestBed.inject(RouteHistoryService);
+    router = TestBed.inject(Router);
+  });
+
+  function expectRoutesToBeUndefined(): void {
+    expect(service.currentRoute).toBeUndefined();
+    expect(service.previousRoute).toBeUndefined();
+  }
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should provide the current route after navigation', async () => {
+    expectRoutesToBeUndefined();
+    await router.navigate(['/home']);
+    expect(service.currentRoute).toEqual('/home');
+    expect(service.previousRoute).toBeUndefined();
+  });
+
+  it('should provide the previous route after second navigation', async () => {
+    expectRoutesToBeUndefined();
+    await router.navigate(['/home']);
+    await router.navigate(['/profile']);
+    expect(service.currentRoute).toEqual('/profile');
+    expect(service.previousRoute).toEqual('/home');
+  });
+
+  it('should destroy', async () => {
+    service.ngOnDestroy();
+    await router.navigate(['/home']);
+    await router.navigate(['/profile']);
+    expectRoutesToBeUndefined();
+  });
+});
