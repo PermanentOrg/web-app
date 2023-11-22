c38fd3fbcd06484cec1d0243700733c1b6a63c2a
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2023-11-22 10:23:04 +0200

    PER-8826-archive-members-with-viewer-role-cannot-get-link
    Enable the get link button for viewers, but disable the publish to internet archive button.

--- src/app/file-browser/components/publish/publish.component.spec.ts
+++ src/app/file-browser/components/publish/publish.component.spec.ts
@@ -1,25 +1,71 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+/* @format */
+import { Shallow } from 'shallow-render';
+import { PublishComponent } from './publish.component';
+import { AccountService } from '@shared/services/account/account.service';
+import { FolderVO, RecordVO } from '@models/index';
+import { FolderResponse } from '@shared/services/api/folder.repo';
+import { Observable } from 'rxjs';
+import { MessageService } from '@shared/services/message/message.service';
+import { ArchiveVO } from '../../../models/archive-vo';
+import { FileBrowserComponentsModule } from '../../file-browser-components.module';
+import { DialogRef, DIALOG_DATA } from '../../../dialog/dialog.service';
+import { ApiService } from '../../../shared/services/api/api.service';
 
-// import { PublishComponent } from './publish.component';
+const mockAccountService = {
+  getArchive: () => {
+    const archive = new ArchiveVO({ accessRole: 'access.role.viewer' });
+    return archive;
+  },
+};
 
-// describe('PublishComponent', () => {
-//   let component: PublishComponent;
-//   let fixture: ComponentFixture<PublishComponent>;
+const mockApiService = {
+  folder: {
+    copy: (
+      folderVOs: FolderVO[],
+      destination: FolderVO
+    ): Promise<FolderResponse> => {
+      return Promise.resolve(new FolderResponse({}));
+    },
+    navigateLean: (folder: FolderVO): Observable<FolderResponse> => {
+      return new Observable<FolderResponse>();
+    },
+  },
+};
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ PublishComponent ]
-//     })
-//     .compileComponents();
-//   }));
+describe('PublishComponent', () => {
+  let shallow: Shallow<PublishComponent>;
+  let messageShown = false;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PublishComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    shallow = new Shallow(PublishComponent, FileBrowserComponentsModule)
+      .mock(AccountService, mockAccountService)
+      .mock(ApiService, mockApiService)
+      .mock(DIALOG_DATA, {
+        item: { folder_linkType: 'linkType' },
+      })
+      .provide({ provide: DialogRef, useValue: new DialogRef(1, null) })
+      .mock(MessageService, {
+        showError: () => {
+          messageShown = true;
+        },
+      });
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', async () => {
+    const { instance } = await shallow.render();
+    expect(instance).toBeTruthy();
+  });
+
+  it('should disaple the public to internet archive button if the user does not have the correct access role', async () => {
+    const { instance,find,fixture } = await shallow.render();
+    instance.publicItem = new RecordVO({ recordId: 1 });
+    instance.publishIa = null;
+    instance.publicLink = null;
+
+    fixture.detectChanges();
+
+    const button = find('.publish-to-archive');
+
+    expect(button.nativeElement.disabled).toBeTruthy();
+  })
+});
