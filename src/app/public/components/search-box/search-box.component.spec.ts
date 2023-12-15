ffa08fe20da6cf8de71d59ca2ca25fe670a33746
Natalie Martin <natalie@permanent.org>
2023-12-15 14:45:49 -0600

    Add basic SearchBoxComponent test
    The SearchBoxComponent needed one of its properties to be renamed as
    part of the work on Featured Archives. However, since SearchBoxComponent
    had no tests, this change was not considered covered by tests. Fix this
    by creating a basic "it exists" test for the SearchBoxComponent. Ideally
    we'd flesh this out to increase coverage in this component as well, but
    that work can be done when we have actual functional adjustments that
    need to be made to the component. Until then, having this basic test is
    much better than having nothing at all.

---
+++ src/app/public/components/search-box/search-box.component.spec.ts
@@ -0,0 +1,19 @@
+/* @format */
+import { Shallow } from 'shallow-render';
+import { PublicModule } from '@public/public.module';
+import { ApiService } from '@shared/services/api/api.service';
+import { SearchBoxComponent } from './search-box.component';
+
+describe('SearchBoxComponent', () => {
+  let shallow: Shallow<SearchBoxComponent>;
+
+  beforeEach(() => {
+    shallow = new Shallow(SearchBoxComponent, PublicModule);
+    shallow.mock(ApiService, {});
+  });
+
+  it('should exist', async () => {
+    const { instance } = await shallow.render();
+    expect(instance).toBeTruthy();
+  });
+});
