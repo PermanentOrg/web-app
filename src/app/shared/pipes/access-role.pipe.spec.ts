601696da319dfd5656053727910481821ea35ed0
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-01-29 18:10:39 +0200

    Linting

--- src/app/shared/pipes/access-role.pipe.spec.ts
+++ src/app/shared/pipes/access-role.pipe.spec.ts
@@ -14,36 +14,42 @@ describe('GetAltTextPipe', () => {
   it('returns the correct access role for the viewer', () => {
     const value = 'access.role.viewer';
     const result = pipe.transform(value);
+
     expect(result).toEqual('Viewer');
   });
 
   it('returns the correct access role for the owner', () => {
     const value = 'access.role.owner';
     const result = pipe.transform(value);
+
     expect(result).toEqual('Owner');
   });
 
   it('returns the correct access role for the curator', () => {
     const value = 'access.role.curator';
     const result = pipe.transform(value);
+
     expect(result).toEqual('Curator');
   });
 
   it('returns the correct access role for the contributor', () => {
     const value = 'access.role.contributor';
     const result = pipe.transform(value);
+
     expect(result).toEqual('Contributor');
   });
 
   it('returns the correct access role for the manager', () => {
     const value = 'access.role.manager';
     const result = pipe.transform(value);
+
     expect(result).toEqual('Manager');
   });
 
   it('returns the correct access role for the editor', () => {
     const value = 'access.role.editor';
     const result = pipe.transform(value);
+
     expect(result).toEqual('Editor');
   });
 });
