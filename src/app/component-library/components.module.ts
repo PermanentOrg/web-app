a78cbb8a7ffb460474872a316dfff4f6abb7aa29
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-02-23 19:49:18 +0200

    This should fix the build error following the merge of the input

--- src/app/component-library/components.module.ts
+++ src/app/component-library/components.module.ts
@@ -10,11 +10,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormInputComponent } from './components/form-input/form-input.component';
 import { ButtonComponent } from './components/button/button.component';
 import { ToggleComponent } from './components/toggle/toggle.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  declarations: [FormInputComponent, ToggleComponent, ButtonComponent],
-  imports: [CommonModule, FontAwesomeModule, FormsModule, ReactiveFormsModule],
-  exports: [FormInputComponent],
+  declarations: [ToggleComponent, ButtonComponent, FormInputComponent],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+  ],
+  exports: [],
 })
 export class ComponentsModule {
   constructor(private library: FaIconLibrary) {
--- src/app/component-library/components/toggle/toggle.stories.ts
+++ src/app/component-library/components/toggle/toggle.stories.ts
@@ -1,12 +1,9 @@
 /* @format */
 import {
-  componentWrapperDecorator,
   Meta,
-  moduleMetadata,
   StoryObj,
 } from '@storybook/angular';
 import { action } from '@storybook/addon-actions';
-import { ComponentsModule } from '../../components.module';
 import { ToggleComponent } from './toggle.component';
 
 const meta: Meta<ToggleComponent> = {
