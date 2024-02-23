249ce4fd4b8af677630b36f9aaa89da059fd9ff4
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-02-23 19:55:14 +0200

    linting

--- src/app/component-library/components.module.ts
+++ src/app/component-library/components.module.ts
@@ -7,10 +7,10 @@ import {
 } from '@fortawesome/angular-fontawesome';
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormInputComponent } from './components/form-input/form-input.component';
 import { ButtonComponent } from './components/button/button.component';
 import { ToggleComponent } from './components/toggle/toggle.component';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [ToggleComponent, ButtonComponent, FormInputComponent],
