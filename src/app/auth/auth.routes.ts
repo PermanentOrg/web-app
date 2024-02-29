c546dc1de34bdbc12689fb8d37f7f9bede1b627e
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-02-29 13:41:26 +0200

    PER-9491: New sign in page
    - Created the new sign in page (kept the old structure but used the new components from the new component library)
    - Some minor changes to the new components because without the design they looked done, but now they are more polished and ready for the design

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -12,6 +12,7 @@ import { MfaComponent } from '@auth/components/mfa/mfa.component';
 import { LoginComponent } from '@auth/components/login/login.component';
 import { ForgotPasswordComponent } from '@auth/components/forgot-password/forgot-password.component';
 import { AnnouncementModule } from '../announcement/announcement.module';
+import { ComponentsModule } from '../component-library/components.module';
 import { AuthComponent } from './components/auth/auth.component';
 import { ShareInviteResolveService } from './resolves/share-invite-resolve.service';
 
@@ -63,6 +64,7 @@ const routes: Routes = [
     SharedModule,
     AnnouncementModule,
     RecaptchaModule,
+    ComponentsModule
   ],
   declarations: [
     LoginComponent,
