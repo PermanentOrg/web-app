ade584edb7498538d012d0b23cc66648d415d52c
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-02-29 15:38:09 +0200

    PER-9491-new-sign-in
    New register page design + a new divider component

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -149,12 +149,4 @@ export class LoginComponent {
         }
       });
   }
-
-  onRegisterClick() {
-    console.log('onRegisterClick');
-    this.router.navigate(['../signup'], {
-      relativeTo: this.route,
-      queryParamsHandling: 'merge',
-    });
-  }
 }
