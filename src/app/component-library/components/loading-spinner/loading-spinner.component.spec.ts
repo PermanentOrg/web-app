1174b468133e7cf9d569786fed41b26f5a005286
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-03-04 13:15:03 +0200

    PER-9491: Add loading spinner component

---
+++ src/app/component-library/components/loading-spinner/loading-spinner.component.spec.ts
@@ -0,0 +1,24 @@
+/* @format */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoadingSpinnerComponent } from './loading-spinner.component';
+
+describe('LoadingSpinnerComponent', () => {
+  let component: LoadingSpinnerComponent;
+  let fixture: ComponentFixture<LoadingSpinnerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LoadingSpinnerComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoadingSpinnerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
