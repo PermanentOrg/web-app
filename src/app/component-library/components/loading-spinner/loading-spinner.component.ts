acead2f10d4fc0da791f0eeffac9ac137eafeaef
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-03-04 13:15:03 +0200

    PER-9491: Add loading spinner component

---
+++ src/app/component-library/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,9 @@
+/* @format */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pr-loading-spinner',
+  templateUrl: './loading-spinner.component.html',
+  styleUrl: './loading-spinner.component.scss',
+})
+export class LoadingSpinnerComponent {}
