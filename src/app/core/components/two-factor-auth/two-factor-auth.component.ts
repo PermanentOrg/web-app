bb19a721fd23c79be2046633a2dfbe9b1956d5d7
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-04-21 15:10:41 +0300

    Added the turn on button and the dropdown

--- src/app/core/components/two-factor-auth/two-factor-auth.component.ts
+++ src/app/core/components/two-factor-auth/two-factor-auth.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pr-two-factor-auth',
   templateUrl: './two-factor-auth.component.html',
-  styleUrl: './two-factor-auth.component.scss'
+  styleUrl: './two-factor-auth.component.scss',
 })
 export class TwoFactorAuthComponent {
-
+  turnOn: boolean = false;
+  method: string = '';
 }
