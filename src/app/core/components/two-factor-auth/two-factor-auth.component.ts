87331a0a1af05e74a855d69e86597bb107e3a636
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-04-23 11:03:03 +0300

    PER-9191- display the methods table with dummy data
    Display the account methods with dummy data in the table. This will be replaced with the api calls once the api is finished.

--- src/app/core/components/two-factor-auth/two-factor-auth.component.ts
+++ src/app/core/components/two-factor-auth/two-factor-auth.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 
+interface Method {
+  id: string;
+  name: string;
+  value: string;
+}
 @Component({
   selector: 'pr-two-factor-auth',
   templateUrl: './two-factor-auth.component.html',
@@ -8,4 +14,30 @@ import { Component } from '@angular/core';
 export class TwoFactorAuthComponent {
   turnOn: boolean = false;
   method: string = '';
+  private form: UntypedFormGroup;
+  methods: Method[] = [];
+  selectedMethodToDelete: Method;
+
+  methodsDictionary = {
+    sms: 'SMS Text',
+    email: 'Email',
+  };
+
+  constructor(private fb: UntypedFormBuilder) {
+    this.methods = [
+      {
+        id: 'email',
+        name: 'email',
+        value: 'email',
+      },
+    ];
+    this.form = fb.group({
+      code: [''],
+      contactInfo: [''],
+    });
+  }
+
+  removeMethod(method: Method): void {
+    this.selectedMethodToDelete = method;
+  }
 }
