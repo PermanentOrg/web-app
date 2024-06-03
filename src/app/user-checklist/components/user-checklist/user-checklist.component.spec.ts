38468343b855e766aafea80782b51ad100f5d3d4
Natalie Martin <natalie@permanent.org>
2024-06-03 14:59:03 -0700

    Set up stories for UserChecklistComponent
    Set up storybook. We probably want stories to test invalid states, which
    means being able to control mock we're passing to the component.
    
    PER-9551: Build checklist UI

--- src/app/user-checklist/components/user-checklist/user-checklist.component.spec.ts
+++ src/app/user-checklist/components/user-checklist/user-checklist.component.spec.ts
@@ -1,28 +1,12 @@
 /* @format */
 import { Shallow } from 'shallow-render';
-import { CHECKLIST_API, ChecklistApi } from '../../types/checklist-api';
+import { CHECKLIST_API } from '../../types/checklist-api';
 import { ChecklistItem } from '../../types/checklist-item';
 import { UserChecklistModule } from '../../user-checklist.module';
 import { UserChecklistComponent } from './user-checklist.component';
+import { DummyChecklistApi } from './shared-mocks';
 
-class DummyChecklistApi implements ChecklistApi {
-  public static error: boolean = false;
-  public static items: ChecklistItem[] = [];
-
-  public static reset(): void {
-    this.items = [];
-    this.error = false;
-  }
-
-  public async getChecklistItems(): Promise<ChecklistItem[]> {
-    if (DummyChecklistApi.error) {
-      throw new Error('Unit test forced error');
-    }
-    return DummyChecklistApi.items;
-  }
-}
-
-fdescribe('UserChecklistComponent', () => {
+describe('UserChecklistComponent', () => {
   let shallow: Shallow<UserChecklistComponent>;
 
   function createTestTask(data?: Partial<ChecklistItem>): ChecklistItem {
