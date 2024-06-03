2587f60ac41203a29b43ee954e2cb8aa2f95135e
Natalie Martin <natalie@permanent.org>
2024-06-03 13:07:40 -0700

    Fetch and display task names in user checklist
    Fetch the checklist data from the API interface and display them in the
    component. Do not display icons or completed status yet.
    
    PER-9551: Build checklist UI

--- src/app/user-checklist/components/user-checklist/user-checklist.component.spec.ts
+++ src/app/user-checklist/components/user-checklist/user-checklist.component.spec.ts
@@ -1,18 +1,57 @@
 /* @format */
 import { Shallow } from 'shallow-render';
+import { CHECKLIST_API, ChecklistApi } from '../../types/checklist-api';
+import { ChecklistItem } from '../../types/checklist-item';
 import { UserChecklistModule } from '../../user-checklist.module';
 import { UserChecklistComponent } from './user-checklist.component';
 
-describe('UserChecklistComponent', () => {
+class DummyChecklistApi implements ChecklistApi {
+  public static items: ChecklistItem[] = [];
+
+  public static reset(): void {
+    this.items = [];
+  }
+
+  public async getChecklistItems(): Promise<ChecklistItem[]> {
+    return DummyChecklistApi.items;
+  }
+}
+
+fdescribe('UserChecklistComponent', () => {
   let shallow: Shallow<UserChecklistComponent>;
 
   beforeEach(async () => {
-    shallow = new Shallow(UserChecklistComponent, UserChecklistModule);
+    shallow = new Shallow(
+      UserChecklistComponent,
+      UserChecklistModule
+    ).provideMock({
+      provide: CHECKLIST_API,
+      useClass: DummyChecklistApi,
+    });
   });
 
   it('should create', async () => {
     const { instance } = await shallow.render();
 
     expect(instance).toBeTruthy();
+  });
+
+  it('should list all tasks received from the API', async () => {
+    DummyChecklistApi.items = [
+      {
+        id: 'test_task',
+        title: 'Write a unit test',
+        completed: true,
+      },
+      {
+        id: 'test_task2',
+        title: 'Write production code',
+        completed: false,
+      },
+    ];
+    const { element } = await shallow.render();
+
+    expect(element.nativeElement.innerText).toContain('Write a unit test');
+    expect(element.nativeElement.innerText).toContain('Write production code');
   });
 });
