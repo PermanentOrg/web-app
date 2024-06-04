9bc1203b93158b3dc32a9496c2b25df22db6806c
Natalie Martin <natalie@permanent.org>
2024-06-04 08:03:52 -0700

    Add checklist icon and style checklist open button
    To make this work in Storybook, it's actually best to include the SVG
    directly in the HTML. To do this cleanly, make the checklist icon its
    own component included in the UserChecklistModule.
    
    PER-9551: Build checklist UI

--- src/app/user-checklist/user-checklist.module.ts
+++ src/app/user-checklist/user-checklist.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserChecklistComponent } from './components/user-checklist/user-checklist.component';
+import { ChecklistIconComponent } from './components/checklist-icon/checklist-icon.component';
 
 @NgModule({
-  declarations: [UserChecklistComponent],
+  declarations: [UserChecklistComponent, ChecklistIconComponent],
   exports: [UserChecklistComponent],
   imports: [CommonModule],
 })
