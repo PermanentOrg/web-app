f62ee7f8edc25c0776b1f1e86414b2d2cc83f4bb
Natalie Martin <natalie@permanent.org>
2024-06-04 09:23:28 -0700

    Add and use TaskIconComponent
    This component contains all of the SVG icons used in the new member
    checklist. It supports all of the currently defined icons and deals with
    styling them if they are completed or not.
    
    PER-9551: Build checklist UI

---
+++ src/app/user-checklist/components/task-icon/task-icon.component.spec.ts
@@ -0,0 +1,60 @@
+import { Shallow } from 'shallow-render';
+import { UserChecklistModule } from '../../user-checklist.module';
+import { TaskIconComponent } from './task-icon.component';
+
+describe('TaskIconComponent', () => {
+  let shallow: Shallow<TaskIconComponent>;
+
+  beforeEach(async () => {
+    shallow = new Shallow(TaskIconComponent, UserChecklistModule);
+  });
+
+  it('does nothing with no icon input', async () => {
+    const { find } = await shallow.render();
+
+    expect(find('.completed').length).toBe(0);
+    expect(find('svg').length).toBe(0);
+  });
+
+  it('can handle undefined icons', async () => {
+    const { find } = await shallow.render({
+      bind: { icon: 'undefinedIconForUnitTest' },
+    });
+
+    expect(find('svg').length).toBe(0);
+  });
+
+  it('should mark the element as completed if specified in the input', async () => {
+    const { find } = await shallow.render({
+      bind: { completed: true },
+    });
+
+    expect(find('.completed').length).toBe(1);
+  });
+
+  describe('defined icons', () => {
+    async function expectIconToHaveDefinedSvg(icon: string) {
+      const { find } = await shallow.render({
+        bind: {
+          icon,
+        },
+      });
+
+      expect(find('svg').length).toBe(1);
+    }
+    const icons = [
+      'archiveCreated',
+      'storageRedeemed',
+      'legacyContact',
+      'archiveSteward',
+      'archiveProfile',
+      'firstUpload',
+      'publishContent',
+    ];
+    icons.forEach((icon) => {
+      it(`has an icon for the "${icon}" item`, async () => {
+        await expectIconToHaveDefinedSvg(icon);
+      });
+    });
+  });
+});
---
+++ src/app/user-checklist/components/task-icon/task-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'pr-task-icon',
+  templateUrl: './task-icon.component.html',
+  styleUrl: './task-icon.component.scss',
+})
+export class TaskIconComponent {
+  @Input() public icon: string = '';
+  @Input() public completed: boolean = false;
+}
