5afb254ca4f0c138a693b0832c7027ae78f4fb57
Natalie Martin <natalie@permanent.org>
2024-08-13 13:57:58 -0700

    Create GetThumbnail function
    This function gets the thumbnail of a record or folder that matches a
    specific minimum size. The intention is to generalize thumbnail logic so
    that when we start generating 256x256 thumbnails, all code using old
    thumbnails can switch over to them without any issues. This is a
    function so that it can be used by RecordVOs and FolderVOs.
    
    PER-9671: Update web-app to handle new thumbnail sizes

---
+++ src/app/models/get-thumbnail.spec.ts
@@ -0,0 +1,53 @@
+import { GetThumbnail } from './get-thumbnail';
+import { FolderVO, RecordVO } from '.';
+
+describe('GetThumbnail', () => {
+  it('returns undefined if no thumbnails are defined', () => {
+    const record = new RecordVO({});
+
+    expect(GetThumbnail(record, 256)).toBeUndefined();
+  });
+
+  it('should return the thumbnail if only one is defined', () => {
+    const record = new RecordVO({ thumbURL200: 'https://example.com' });
+
+    expect(GetThumbnail(record, 1000)).toBe('https://example.com');
+  });
+
+  it('should return the thumbnail if the only one defined is not the minimum size', () => {
+    const record = { thumbURL2000: 'https://example.com' };
+
+    expect(GetThumbnail(record, Infinity)).toBe('https://example.com');
+  });
+
+  it('should return the correct thumbnail if two are defined', () => {
+    const record = new RecordVO({
+      thumbURL200: 'https://example.com/invalid',
+      thumbURL500: 'https://example.com/correct',
+    });
+
+    expect(GetThumbnail(record, 300)).toBe('https://example.com/correct');
+  });
+
+  it('should identify the correct thumbnail if all are defined', () => {
+    const record = new RecordVO({
+      thumbURL200: 'https://example.com/invalid',
+      thumbURL500: 'https://example.com/invalid',
+      thumbURL1000: 'https://example.com/correct',
+      thumbURL2000: 'https://example.com/invalid',
+    });
+
+    expect(GetThumbnail(record, 1000)).toBe('https://example.com/correct');
+  });
+
+  it('can take in a folder as well', () => {
+    const folder = new FolderVO({
+      thumbURL200: 'https://example.com/invalid',
+      thumbURL500: 'https://example.com/invalid',
+      thumbURL1000: 'https://example.com/correct',
+      thumbURL2000: 'https://example.com/invalid',
+    });
+
+    expect(GetThumbnail(folder, 1000)).toBe('https://example.com/correct');
+  });
+});
---
+++ src/app/models/get-thumbnail.ts
@@ -0,0 +1,26 @@
+interface ThumbnailData {
+  size: number;
+  url: string;
+}
+export interface HasThumbnails {
+  thumbURL200?: string;
+  thumbURL500?: string;
+  thumbURL1000?: string;
+  thumbURL2000?: string;
+}
+
+export function GetThumbnail(
+  item: HasThumbnails,
+  minimumSize: number,
+): string | undefined {
+  const thumbnails: ThumbnailData[] = [
+    { size: 200, url: item.thumbURL200 },
+    { size: 500, url: item.thumbURL500 },
+    { size: 1000, url: item.thumbURL1000 },
+    { size: 2000, url: item.thumbURL2000 },
+  ].filter((thumb) => thumb.url);
+  return (
+    thumbnails.find((thumb) => thumb.size >= minimumSize)?.url ||
+    thumbnails[0]?.url
+  );
+}
