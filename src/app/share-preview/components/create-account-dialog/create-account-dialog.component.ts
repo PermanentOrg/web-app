a7adc8e466ed8f9dfd2dd55c4d15eed38011e129
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-09-18 16:19:54 +0300

    Save the token in the local storage

--- src/app/share-preview/components/create-account-dialog/create-account-dialog.component.ts
+++ src/app/share-preview/components/create-account-dialog/create-account-dialog.component.ts
@@ -1,21 +1,29 @@
 /* @format */
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { DeviceService } from '@shared/services/device/device.service';
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'pr-create-account-dialog',
   templateUrl: './create-account-dialog.component.html',
   styleUrls: ['./create-account-dialog.component.scss'],
 })
-export class CreateAccountDialogComponent {
+export class CreateAccountDialogComponent implements OnInit {
   sharerName: string = this.data.sharerName;
 
   constructor(
     private dialogRef: DialogRef,
     private device: DeviceService,
+    private route: ActivatedRoute,
     @Inject(DIALOG_DATA) public data: any,
   ) {}
+
+  ngOnInit(): void {
+    const url = window.location.href;
+    const token = url.split('/').pop();
+    localStorage.setItem('shareToken', token);
+  }
 
   isMobile(): boolean {
     return this.device.isMobileWidth();
