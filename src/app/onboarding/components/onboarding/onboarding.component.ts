a84069369d48eebd85050d979923ff8a714f3555
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-10-16 01:04:02 +0300

    redirect to shares from if there is an invitation

--- src/app/onboarding/components/onboarding/onboarding.component.ts
+++ src/app/onboarding/components/onboarding/onboarding.component.ts
@@ -88,11 +88,12 @@ export class OnboardingComponent implements OnInit {
       this.selectedPendingArchive = null;
     }
     if (screen === OnboardingScreen.done) {
-      if (localStorage.getItem('shareToken')) {
-        localStorage.removeItem('shareToken');
-      }
       if (this.acceptedInvite) {
         this.router.navigate(['/app', 'welcome-invitation']);
+      }
+      if (localStorage.getItem('shareToken')) {
+        localStorage.removeItem('shareToken');
+        this.router.navigate(['/app', 'shares']);
       } else {
         this.router.navigate(['/app', 'welcome']);
       }
