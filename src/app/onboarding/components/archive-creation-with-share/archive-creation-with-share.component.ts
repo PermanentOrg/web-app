6762548a8852762c95d0919ed5aadbb2bdde8c36
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2024-10-25 02:46:56 +0300

    PER-98890-fix-share-link-invite
    fix the share link invate workflow

--- src/app/onboarding/components/archive-creation-with-share/archive-creation-with-share.component.ts
+++ src/app/onboarding/components/archive-creation-with-share/archive-creation-with-share.component.ts
@@ -17,6 +17,7 @@ export class ArchiveCreationWithShareComponent implements OnInit {
 
   ngOnInit(): void {
     const token = localStorage.getItem('shareToken');
+    const copyToken = localStorage.getItem('shareTokenFromCopy');
     if (token) {
       this.api.invite.getFullShareInvite(token).then((invite) => {
         const inviteVO = invite.getInviteVO();
@@ -25,6 +26,14 @@ export class ArchiveCreationWithShareComponent implements OnInit {
           ? inviteVO.RecordVO.displayName
           : inviteVO.FolderVO.displayName;
         this.isFolder = !inviteVO.RecordVO;
+      });
+    } else if (copyToken) {
+      this.api.share.checkShareLink(copyToken).then((invite) => {
+        const inviteVO = invite.Results[0].data[0].Shareby_urlVO;
+        this.sharerName = inviteVO.AccountVO.fullName;
+        this.sharedItemName = inviteVO.RecordVO
+          ? inviteVO.RecordVO.displayName
+          : inviteVO.FolderVO.displayName;
       });
     }
   }
