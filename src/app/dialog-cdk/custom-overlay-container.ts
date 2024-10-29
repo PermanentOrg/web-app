0a3a12a7d9b0d9f99723f53f027bfc240b2690e4
Natalie Martin <natalie@permanent.org>
2024-10-29 09:56:12 -0700

    Fix CustomOverlayContainer test failures
    These tests were utilizing the Document object in ways that were not
    perfectly isolated. While they had some cleanup logic, other tests may
    manipulate the Document in such a way that make these tests fail. Fix
    this by allowing tests to inject a node to search for instead of
    defaulting to `document.body`. The `document.body` node is used in the
    actual `_createContainer()` method.
    
    PER-9864: CustomOverlayContainer test failure

--- src/app/dialog-cdk/custom-overlay-container.ts
+++ src/app/dialog-cdk/custom-overlay-container.ts
@@ -4,16 +4,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CustomOverlayContainer extends OverlayContainer {
-  protected _createContainer(): void {
+  public appendContainer(node: Element): void {
     const container = document.createElement('div');
     container.classList.add('cdk-overlay-container');
 
-    const parent = document.querySelector('pr-app-root') || document.body;
-    if (parent) {
-      parent.appendChild(container);
-    } else {
-      document.body.appendChild(container);
-    }
+    const parent = node.querySelector('pr-app-root') || node;
+    parent.appendChild(container);
     this._containerElement = container;
+  }
+
+  protected _createContainer(): void {
+    this.appendContainer(document.body);
   }
 }
