13042705db3295185b3675650b7365edf236047d
Natalie Martin <natalie@permanent.org>
2025-02-03 07:33:39 -0700

    Link to dialogs/help on checklist items
    Create a pipe so that the HTML template can look up whether a task has a
    defined link or not. Querying the pipe within the template multiple
    times is awkward, but it is preferred to storing the data in component
    properties.
    
    PER-9973: Users can get help with checklist items when clicked

---
+++ src/app/user-checklist/pipes/task-link.pipe.ts
@@ -0,0 +1,46 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+export interface TaskRouterLink {
+  routerLink: (string | { outlets: { dialog: string[] } })[] | string;
+  fragment?: string;
+  external?: boolean;
+}
+
+@Pipe({
+  name: 'taskLink',
+})
+export class TaskLinkPipe implements PipeTransform {
+  private readonly links: Record<string, TaskRouterLink> = {
+    storageRedeemed: {
+      routerLink: [{ outlets: { dialog: ['storage', 'add'] } }],
+      fragment: 'promo',
+    },
+    legacyContact: {
+      routerLink: [{ outlets: { dialog: ['account'] } }],
+      fragment: 'legacy-contact',
+    },
+    archiveSteward: {
+      routerLink: [{ outlets: { dialog: ['settings'] } }],
+      fragment: 'legacy-planning',
+    },
+    archiveProfile: {
+      routerLink: [{ outlets: { dialog: ['profile'] } }],
+    },
+    firstUpload: {
+      routerLink:
+        'https://permanent.zohodesk.com/portal/en/kb/articles/upload-your-first-file',
+      external: true,
+    },
+    publishContent: {
+      routerLink:
+        'https://permanent.zohodesk.com/portal/en/kb/articles/upload-your-first-file',
+      external: true,
+    },
+  };
+
+  transform(value: string, ..._: unknown[]): TaskRouterLink | undefined {
+    if (this.links[value]) {
+      return this.links[value];
+    }
+  }
+}
--- src/app/user-checklist/user-checklist.module.ts
+++ src/app/user-checklist/user-checklist.module.ts
@@ -1,8 +1,8 @@
-/* @format */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { EventService } from '@shared/services/event/event.service';
+import { RouterModule } from '@angular/router';
 import { UserChecklistComponent } from './components/user-checklist/user-checklist.component';
 import { ChecklistIconComponent } from './components/checklist-icon/checklist-icon.component';
 import { TaskIconComponent } from './components/task-icon/task-icon.component';
@@ -10,6 +10,7 @@ import { MinimizeIconComponent } from './components/minimize-icon/minimize-icon.
 import { CHECKLIST_API } from './types/checklist-api';
 import { UserChecklistService } from './services/user-checklist.service';
 import { ChecklistEventObserverService } from './services/checklist-event-observer.service';
+import { TaskLinkPipe } from './pipes/task-link.pipe';
 
 @NgModule({
   declarations: [
@@ -17,9 +18,10 @@ import { ChecklistEventObserverService } from './services/checklist-event-observ
     ChecklistIconComponent,
     TaskIconComponent,
     MinimizeIconComponent,
+    TaskLinkPipe,
   ],
   exports: [UserChecklistComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, RouterModule],
   providers: [
     {
       provide: CHECKLIST_API,
