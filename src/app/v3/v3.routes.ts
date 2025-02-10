8cb461c4bdc92ba7d1683c85ab99803d47e594d0
Natalie Martin <natalie@permanent.org>
2025-02-10 07:11:16 -0700

    Create "v3" section of the app
    Create a new Angular module with a basic placeholer root component. Set
    up routing and let it be lazy-loaded.
    
    PER-10019: Set up v3 section of project

---
+++ src/app/v3/v3.routes.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { V3RootComponent } from './components/v3-root/v3-root.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: V3RootComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class V3RoutingModule {}
