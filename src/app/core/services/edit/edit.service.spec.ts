1b055a7c691596ad56125d412d9a8cc312022b08
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2025-03-18 18:04:15 +0200

    remove unused tests

--- src/app/core/services/edit/edit.service.spec.ts
+++ src/app/core/services/edit/edit.service.spec.ts
@@ -66,123 +66,21 @@ describe('EditService', () => {
     expect(record.updatedDT).toBe('2024-03-03');
   });
 
-  it('should not call update when records list is empty', async () => {
-    const mockRecords: RecordVO[] = [];
-    const promises = [];
-
-    if (mockRecords.length) {
-      const archiveId = accountService.getArchive().archiveId;
-      promises.push(apiService.record.update(mockRecords, archiveId));
-    } else {
-      promises.push(Promise.resolve());
-    }
-
-    await Promise.all(promises);
-
-    expect(apiService.record.update).not.toHaveBeenCalled();
-  });
-
-  it('should call update and process response when records exist', async () => {
-    const mockRecords = [
-      new RecordVO({ recordId: 1, folder_linkId: 10 }),
-      new RecordVO({ recordId: 2, folder_linkId: 11 }),
-    ];
-
-    const mockResponse = [
-      { updatedDT: '2024-03-03', TimezoneVO: { timezone: 'UTC' } },
-    ];
-
-    (apiService.record.update as jasmine.Spy).and.returnValue(
-      Promise.resolve(mockResponse),
-    );
-
-    const promises = [];
-
-    if (mockRecords.length) {
-      const archiveId = accountService.getArchive().archiveId;
-      promises.push(apiService.record.update(mockRecords, archiveId));
-    } else {
-      promises.push(Promise.resolve());
-    }
-
-    const results = await Promise.all(promises);
-    const recordResponse = results[0];
-
-    expect(apiService.record.update).toHaveBeenCalledWith(mockRecords, 123);
-    expect(recordResponse[0].updatedDT).toBe('2024-03-03');
-    expect(recordResponse[0].TimezoneVO.timezone).toBe('UTC');
-  });
-
   it('should handle empty records array and not call update', async () => {
     const mockRecords: RecordVO[] = [];
-    const promises = [];
 
-    if (mockRecords.length) {
-      const archiveId = accountService.getArchive().archiveId;
-      promises.push(apiService.record.update(mockRecords, archiveId));
-    } else {
-      promises.push(Promise.resolve());
-    }
-
-    await Promise.all(promises);
+    await service.updateItems(mockRecords);
 
     expect(apiService.record.update).not.toHaveBeenCalled();
   });
 
-  it('should call update when records exist', async () => {
-    const mockRecords = [new RecordVO({ recordId: 1, displayName: 'Test' })];
-    const archiveId = 123;
-    const mockResponse = [{ updatedDT: '2024-03-03' }];
-
-    accountService.getArchive.and.returnValue(new ArchiveVO({ archiveId }));
-    (apiService.record.update as jasmine.Spy).and.returnValue(
-      Promise.resolve(mockResponse),
-    );
-
-    const promises = [];
-    if (mockRecords.length) {
-      const archiveId = accountService.getArchive().archiveId;
-      promises.push(apiService.record.update(mockRecords, archiveId));
-    } else {
-      promises.push(Promise.resolve());
-    }
-
-    const results = await Promise.all(promises);
-
-    expect(apiService.record.update).toHaveBeenCalledWith(
-      mockRecords,
-      archiveId,
-    );
-
-    expect(results[0][0].updatedDT).toBe('2024-03-03');
-  });
-
-  it('should NOT update record when recordResponse is empty', () => {
+  it('should NOT update record when recordResponse is empty', async () => {
     const recordMock = new RecordVO({ recordId: 1, folder_linkId: 10 });
     recordMock.update = jasmine.createSpy('update');
 
-    const mockResponse = []; // Empty response
-    const itemsByLinkId = { 10: recordMock };
-    const recordsByRecordId = new Map<number, RecordVO>();
+    await service.updateItems([recordMock]);
 
-    if (mockResponse.length) {
-      const res = mockResponse[0];
-
-      const newData: RecordVOData = {
-        updatedDT: res.updatedDT,
-      };
-
-      if (res.TimezoneVO) {
-        newData.TimezoneVO = res.TimezoneVO;
-      }
-
-      const record =
-        (itemsByLinkId[res.folder_linkId] as RecordVO) ||
-        recordsByRecordId.get(res.recordId);
-
-      record.update(newData);
-    }
-
+    expect(apiService.record.update).toHaveBeenCalled();
     expect(recordMock.update).not.toHaveBeenCalled();
   });
 });
