7a0b8c60af922268e42330516c9ea885f38180b8
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2025-03-19 15:30:47 +0200

    Add new test

--- src/app/shared/services/api/invite.repo.spec.ts
+++ src/app/shared/services/api/invite.repo.spec.ts
@@ -7,7 +7,7 @@ import { environment } from '@root/environments/environment';
 
 import { HttpService } from '@shared/services/http/http.service';
 import { InviteRepo, InviteResponse } from '@shared/services/api/invite.repo';
-import { InviteVO, RecordVO } from '@root/app/models';
+import { FolderVO, InviteVO, RecordVO } from '@root/app/models';
 import { HttpV2Service } from '../http-v2/http-v2.service';
 
 describe('InviteRepo', () => {
@@ -49,7 +49,7 @@ describe('InviteRepo', () => {
     req.flush(expected);
   });
 
-  it('should send a share invite successfully', () => {
+  it('should send a share invite for a record successfully', () => {
     const mockResponse = {
       inviteId: 8,
       email: 'john.doe@gmail.com',
@@ -105,6 +105,67 @@ describe('InviteRepo', () => {
       folderLinkId: 456,
       relationship: 'relation.family.uncle',
       recordId: 789,
+    });
+
+    req.flush(mockResponse);
+  });
+
+  it('should send a share invite for a folder successfully', () => {
+    const mockResponse = {
+      inviteId: 8,
+      email: 'john.doe@gmail.com',
+      byArchiveId: 123,
+      byAccountId: 2,
+      expiresDT: '2025-04-18T12:42:55',
+      fullName: 'John Doe',
+      message: null,
+      relationship: 'relation.family.uncle',
+      accessRole: 'access.role.viewer',
+      timesSent: 1,
+      giftSizeInMB: null,
+      token: 'token',
+      status: 'status.invite.pending',
+      type: 'type.invite.share',
+      FolderVO: null,
+      RecordVO: null,
+      ArchiveVO: null,
+      AccountVO: null,
+      InviteShareVO: null,
+      ShareVO: null,
+      createdDT: '2025-03-19T12:42:55',
+      updatedDT: '2025-03-19T12:42:55',
+      recordId: 789,
+    };
+
+    const invite = new InviteVO({
+      fullName: 'John Doe',
+      email: 'john.doe@gmail.com',
+      byArchiveId: 123,
+      accessRole: 'access.role.viewer',
+      relationship: 'relation.family.uncle',
+    });
+
+    const itemToShare = new FolderVO({
+      folder_linkId: 456,
+      folderId: 789,
+    });
+
+    repo.sendShareInvite(invite, itemToShare).then((response) => {
+      expect(response).toEqual([mockResponse]);
+    });
+
+    const req = httpMock.expectOne(`${environment.apiUrl}/invite/share`);
+
+    expect(req.request.method).toBe('POST');
+
+    expect(req.request.body).toEqual({
+      email: 'john.doe@gmail.com',
+      byArchiveId: 123,
+      fullName: 'John Doe',
+      accessRole: 'access.role.viewer',
+      folderLinkId: 456,
+      relationship: 'relation.family.uncle',
+      folderId: 789,
     });
 
     req.flush(mockResponse);
