a4a6eff19fc788a5a1cc9885dcb9b080e3b715f0
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2025-04-29 21:11:12 +0300

    prettier

--- src/app/file-browser/components/file-list-controls/file-list-controls.component.ts
+++ src/app/file-browser/components/file-list-controls/file-list-controls.component.ts
@@ -493,7 +493,6 @@ export class FileListControlsComponent implements OnDestroy, HasSubscriptions {
   }
 
   displayDownloadOptions() {
-    console.log(this.selectedItems)
     this.displayDownloadDropdown = true;
     this.downloadOptions = (
       this.selectedItems[0] as RecordVO
