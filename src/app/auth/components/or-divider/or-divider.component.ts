0e604ba33a6d1e73c998ead7fe70e69d336f23fe
crisnicandrei <62384997+crisnicandrei@users.noreply.github.com>
2025-08-04 13:04:27 +0300

    PER-10230 Rename account and billing tabs

--- src/app/auth/components/or-divider/or-divider.component.ts
+++ src/app/auth/components/or-divider/or-divider.component.ts
@@ -2,7 +2,6 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'pr-or-divider',
-  standalone: false,
   templateUrl: './or-divider.component.html',
   styleUrl: './or-divider.component.scss',
 })
