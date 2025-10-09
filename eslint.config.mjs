97679287b746780ad7081617b46f77d1e4b81567
Dan Schultz <slifty@gmail.com>
2025-10-09 14:46:52 -0400

    Enable `radix`
    Issue #628 Enable disabled lint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -162,7 +162,6 @@ export default typescriptEslint.config([
 			'prefer-promise-reject-errors': 'off',
 			'prefer-regex-literals': 'off',
 			'prefer-template': 'off',
-			radix: 'off',
 			'spaced-comment': 'off',
 			'vars-on-top': 'off',
 			yoda: 'off',
