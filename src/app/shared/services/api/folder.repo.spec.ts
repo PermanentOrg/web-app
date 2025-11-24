4f84e5c5bf429d47d801fe1203f50ad729e1163f
aasandei-vsp <andreea.asandei@vspartners.us>
2025-11-24 14:46:45 +0200

    Add fallback for stela folder calls
    Calls for folders on stela can be made with two types of authentication:
    - auth token
    - share token
    When making a call with the share token, if that folder is not unrestricted,
    the endpoint will return an empty list. When this happens, if the user is
    logged in, we try to make the call for the folder again, using the auth token,
    because maybe the user does have access to it and we can still get it.
    
    Issue: PER-10357 Fallback for share token

--- src/app/shared/services/api/folder.repo.spec.ts
+++ src/app/shared/services/api/folder.repo.spec.ts
@@ -1,39 +1,138 @@
-// This suite doesn't actually have any tests, but for some reason the setup
-// was causing the test bed to get contaminated.
-// For this reason, I comment it out!  There are other tests
-// fully commented out, and we will be cleaning them up
+import { TestBed } from '@angular/core/testing';
+import { FolderVO } from '@models/index';
+import { of } from 'rxjs';
+import { HttpV2Service } from '../http-v2/http-v2.service';
+import { HttpService } from '../http/http.service';
+import { FolderRepo } from './folder.repo';
 
-// import { TestBed } from '@angular/core/testing';
-// import {
-// 	HttpTestingController,
-// 	provideHttpClientTesting,
-// } from '@angular/common/http/testing';
+const emptyResponse = { items: [] };
+// The extra properties irrelevant to this test suite need to be there because
+// there are some checks missing in convertStelaFolderToFolderVO,
+// which will be fixed in the next commit
+const fakeFolderResponse = {
+	items: [
+		{
+			id: 42,
+			name: 'Auth Folder',
+			thumbnailUrls: { 200: 'test' },
+			paths: { names: 'test' },
+			location: { stelaLocation: { id: 13 } },
+		},
+	],
+};
+const fakeChildrenResponse = {
+	items: [
+		{
+			id: 300,
+			name: 'Auth Child',
+			thumbnailUrls: { 200: 'test' },
+			paths: { names: 'test' },
+			location: { stelaLocation: { id: 13 } },
+		},
+	],
+};
 
-// import { HttpService } from '@shared/services/http/http.service';
-// import { FolderRepo } from '@shared/services/api/folder.repo';
-// import {
-// 	provideHttpClient,
-// 	withInterceptorsFromDi,
-// } from '@angular/common/http';
+describe('Folder repo', () => {
+	let folderRepo: FolderRepo;
+	let httpSpy: jasmine.SpyObj<HttpService>;
+	let httpV2Spy: jasmine.SpyObj<HttpV2Service>;
 
-// describe('FolderRepo', () => {
-// 	let repo: FolderRepo;
-// 	let httpMock: HttpTestingController;
+	beforeEach(() => {
+		httpSpy = jasmine.createSpyObj('HttpService', [
+			'sendRequest',
+			'sendRequestPromise',
+		]);
+		httpV2Spy = jasmine.createSpyObj('HttpV2Service', ['get']);
 
-// 	beforeEach(() => {
-// 		TestBed.configureTestingModule({
-// 			imports: [],
-// 			providers: [
-// 				HttpService,
-// 				provideHttpClient(withInterceptorsFromDi()),
-// 				provideHttpClientTesting(),
-// 			],
-// 		});
+		TestBed.configureTestingModule({
+			providers: [
+				FolderRepo,
+				{ provide: HttpService, useValue: httpSpy },
+				{ provide: HttpV2Service, useValue: httpV2Spy },
+			],
+		});
 
-// 		httpMock = TestBed.inject(HttpTestingController);
-// 	});
+		folderRepo = TestBed.inject(FolderRepo);
+	});
 
-// 	afterEach(() => {
-// 		httpMock.verify();
-// 	});
-// });
+	it('should get folder with children using the auth token', async () => {
+		const mockFolderVO = { folderId: 42 } as FolderVO;
+
+		httpV2Spy.get.and.returnValues(
+			of([fakeFolderResponse]),
+			of([fakeChildrenResponse]),
+		);
+
+		const result = await folderRepo.getWithChildren([mockFolderVO]);
+
+		expect(httpV2Spy.get).toHaveBeenCalledWith('v2/folder', {
+			folderIds: [42],
+		});
+
+		expect(httpV2Spy.get).toHaveBeenCalledWith('v2/folder/42/children', {
+			pageSize: 99999999,
+		});
+
+		expect(result.isSuccessful).toBeTrue();
+		expect(result.Results[0].data[0].FolderVO).toBeDefined();
+	});
+
+	it('should get folder with children using the share token', async () => {
+		const mockFolderVO = { folderId: 42 } as FolderVO;
+
+		httpV2Spy.get.and.returnValues(
+			of([fakeFolderResponse]),
+			of([fakeChildrenResponse]),
+		);
+
+		const result = await folderRepo.getWithChildren(
+			[mockFolderVO],
+			'share-token-123',
+		);
+
+		expect(httpV2Spy.get).toHaveBeenCalledWith(
+			'v2/folder',
+			{ folderIds: [42] },
+			null,
+			{ authToken: false, shareToken: 'share-token-123' },
+		);
+
+		expect(httpV2Spy.get).toHaveBeenCalledWith(
+			'v2/folder/42/children',
+			jasmine.anything(),
+			null,
+			{ authToken: false, shareToken: 'share-token-123' },
+		);
+
+		expect(result.Results[0].data[0].FolderVO).toBeDefined();
+	});
+
+	it('should get folder with children using fallback to auth token', async () => {
+		const mockFolderVO = { folderId: 42 } as FolderVO;
+
+		httpV2Spy.get.and.returnValues(
+			of([emptyResponse]),
+			of([fakeFolderResponse]),
+			of([emptyResponse]),
+			of([fakeChildrenResponse]),
+		);
+
+		const result = await folderRepo.getWithChildren(
+			[mockFolderVO],
+			'bad-share-token',
+		);
+
+		expect(httpV2Spy.get).toHaveBeenCalledWith(
+			'v2/folder',
+			{ folderIds: [42] },
+			null,
+			{ authToken: false, shareToken: 'bad-share-token' },
+		);
+
+		expect(httpV2Spy.get).toHaveBeenCalledWith('v2/folder', {
+			folderIds: [42],
+		});
+
+		expect(result.Results[0].data[0].FolderVO).toBeDefined();
+	});
+});
