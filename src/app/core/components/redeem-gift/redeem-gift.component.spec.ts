021789ccde71b387ef5f908bef1537bd0378ac33
Dan Schultz <slifty@gmail.com>
2026-01-09 14:51:35 -0500

    Migrate to ng-mock (Part 5)
    This continues the work of migrating to ng-mocks, but in a separate
    commit for the sake of easier code review.
    
    Issue #879

--- src/app/core/components/redeem-gift/redeem-gift.component.spec.ts
+++ src/app/core/components/redeem-gift/redeem-gift.component.spec.ts
@@ -1,6 +1,8 @@
-import { Shallow } from 'shallow-render';
+import { NgModule } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { MockBuilder, MockRender, ngMocks } from 'ng-mocks';
+import { ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
 import { AccountService } from '@shared/services/account/account.service';
-import { CoreModule } from '@core/core.module';
 import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { PromoVOData } from '../../../models/promo-vo';
@@ -13,8 +15,10 @@ import {
 	MockBillingRepo,
 } from './shared-mocks';
 
+@NgModule()
+class DummyModule {}
+
 describe('StorageDialogComponent', () => {
-	let shallow: Shallow<RedeemGiftComponent>;
 	let mockAccountService: MockAccountService;
 	let mockApiService: MockApiService;
 	let mockActivatedRoute;
@@ -30,35 +34,33 @@ describe('StorageDialogComponent', () => {
 		mockApiService = {
 			billing: new MockBillingRepo(),
 		};
-		shallow = new Shallow(RedeemGiftComponent, CoreModule)
-			.dontMock(AccountService)
-			.dontMock(ApiService)
-			.mock(MessageService, {
-				showError: () => {},
+		return MockBuilder(RedeemGiftComponent, DummyModule)
+			.keep(ReactiveFormsModule, { export: true })
+			.keep(UntypedFormBuilder)
+			.provide({
+				provide: MessageService,
+				useValue: { showError: () => {} },
 			})
 			.provide({ provide: AccountService, useValue: mockAccountService })
 			.provide({ provide: ApiService, useValue: mockApiService })
-			.provideMock([{ provide: ActivatedRoute, useValue: mockActivatedRoute }]);
+			.provide({ provide: ActivatedRoute, useValue: mockActivatedRoute });
 	});
 
-	it('should exist', async () => {
-		const { element } = await shallow.render();
+	it('should exist', () => {
+		const fixture = MockRender(RedeemGiftComponent);
 
-		expect(element).not.toBeNull();
+		expect(fixture.point.nativeElement).not.toBeNull();
 	});
 
-	it('has an input for a prefilled promo code', async () => {
-		const { find } = await shallow.render({
-			bind: {
-				promoCode: 'potato',
-			},
-		});
+	it('has an input for a prefilled promo code', () => {
+		const fixture = MockRender(RedeemGiftComponent, { promoCode: 'potato' });
 
-		expect(find('input').nativeElement.value).toBe('potato');
+		expect(ngMocks.find('input').nativeElement.value).toBe('potato');
 	});
 
 	it('should send an API request when submitting a promo code', async () => {
-		const { instance } = await shallow.render();
+		const fixture = MockRender(RedeemGiftComponent);
+		const instance = fixture.point.componentInstance;
 		const promoData: PromoVOData = { code: 'promo' };
 		await instance.onPromoFormSubmit(promoData);
 
@@ -67,7 +69,8 @@ describe('StorageDialogComponent', () => {
 	});
 
 	it('should update the account after redeeming a promo code', async () => {
-		const { instance } = await shallow.render();
+		const fixture = MockRender(RedeemGiftComponent);
+		const instance = fixture.point.componentInstance;
 		const promoData: PromoVOData = { code: 'promo' };
 		await instance.onPromoFormSubmit(promoData);
 
@@ -75,20 +78,22 @@ describe('StorageDialogComponent', () => {
 		expect(instance.resultMessage.successful).toBeTrue();
 	});
 
-	it('should enable the submit button after adding a promo code', async () => {
-		const { find, instance, fixture } = await shallow.render();
+	it('should enable the submit button after adding a promo code', () => {
+		const fixture = MockRender(RedeemGiftComponent);
+		const instance = fixture.point.componentInstance;
 		instance.promoForm.patchValue({
 			code: 'promo1',
 		});
 		instance.promoForm.updateValueAndValidity();
 		fixture.detectChanges();
-		const button = find('.btn-primary');
+		const button = ngMocks.find('.btn-primary');
 
 		expect(button.nativeElement.disabled).toBeFalsy();
 	});
 
 	it('should handle an invalid promo code', async () => {
-		const { instance } = await shallow.render();
+		const fixture = MockRender(RedeemGiftComponent);
+		const instance = fixture.point.componentInstance;
 		mockApiService.billing.isSuccessful = false;
 		await instance.onPromoFormSubmit({ code: 'potato' });
 
@@ -96,7 +101,8 @@ describe('StorageDialogComponent', () => {
 	});
 
 	it('should handle any other unexpected errors when redeeming promo code', async () => {
-		const { instance } = await shallow.render();
+		const fixture = MockRender(RedeemGiftComponent);
+		const instance = fixture.point.componentInstance;
 		mockAccountService.failRefresh = true;
 		await instance.onPromoFormSubmit({ code: 'potato' });
 
@@ -104,7 +110,8 @@ describe('StorageDialogComponent', () => {
 	});
 
 	it('should not bump up account storage if it has already been done on the server side', async () => {
-		const { instance } = await shallow.render();
+		const fixture = MockRender(RedeemGiftComponent);
+		const instance = fixture.point.componentInstance;
 		mockAccountService.addMoreSpaceAfterRefresh = true;
 		await instance.onPromoFormSubmit({ code: 'potato' });
 
